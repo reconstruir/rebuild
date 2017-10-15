4f17dabce1c48763d17be469f78b14008220d156
Ramiro Estrugo <ramiro@fateware.com>
2017-10-15 12:10:38 -0700

    skel

---
+++ lib/rebuild/source_finder/source_finder.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from abc import abstractmethod, ABCMeta
+from bes.system.compat import with_metaclass
+
+class source_finder(with_metaclass(ABCMeta, object)):
+
+  @abstractmethod
+  def find_tarball(self, name, version, build_target):
+    pass
