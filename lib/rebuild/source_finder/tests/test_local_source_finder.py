2d7f712ed6d487bf53a5ea663d83bf6daa7eba91
Ramiro Estrugo <ramiro@fateware.com>
2017-10-16 15:54:45 -0700

    move test code to its own class

--- lib/rebuild/source_finder/tests/test_local_source_finder.py
+++ lib/rebuild/source_finder/tests/test_local_source_finder.py
@@ -2,84 +2,107 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from bes.testing.unit_test import unit_test
+from bes.common import string_util
 from bes.fs.testing import temp_content
 from bes.fs import temp_file
-from bes.archive.temp_archive import temp_archive
+from bes.archive.temp_archive import archive_extension, temp_archive
 from rebuild.source_finder import local_source_finder
 import os.path as path
 
-class test_local_finder(unit_test):
+class source_dir_maker(object):
 
+  @classmethod
+  def make(clazz, items):
+    items = [ clazz._munge_item(item) for item in items ]
+    return temp_content.write_items_to_temp_dir(items)
+
+  @classmethod
+  def _munge_item(clazz, item):
+    ext = clazz._item_ext(item)
+    assert ext
+    tarball = clazz._tarball_for_ext(ext)
+    return item.replace('${tarball}', tarball)
+    
+  @classmethod
+  def _item_ext(clazz, item):
+    filename = string_util.split_by_white_space(item, strip = True)[1]
+    return archive_extension.extension_for_filename(filename)
+
+  _TARBALLS = {}
+  @classmethod
+  def _tarball_for_ext(clazz, ext):
+    if not ext in clazz._TARBALLS:
+      clazz._TARBALLS[ext] = clazz._make_tarball(ext)
+    return clazz._TARBALLS[ext]
+  
   # Any tarball will do for the tests as long as the file type is an archive
-  def _make_tarball(ext):
+  @classmethod
+  def _make_tarball(clazz, ext):
     return temp_archive.make_temp_archive([ temp_archive.Item('foo.txt', content = 'foo.txt\n') ], ext).filename
-  
-  BZ2_TARBALL = _make_tarball('bz2')
-  TAR_GZ_TARBALL = _make_tarball('tar.gz')
-  TGZ_TARBALL = _make_tarball('tgz')
-  ZIP_TARBALL = _make_tarball('zip')
-  
+
+class test_local_source_finder(unit_test):
+
   def test_local_finder_tar_gz(self):
-    tmp_source_dir = temp_content.write_items_to_temp_dir([
-      'file a/alpha-1.2.3.tar.gz "%s" 644' % (self.TAR_GZ_TARBALL),
-      'file b/beta-1.2.3.tar.gz "%s" 644' % (self.TAR_GZ_TARBALL),
+    tmp_source_dir = source_dir_maker.make([
+      'file a/alpha-1.2.3.tar.gz "${tarball}" 644',
+      'file b/beta-1.2.3.tar.gz "${tarball}" 644',
     ])
     finder = local_source_finder(tmp_source_dir)
     self.assertEqual( path.join(tmp_source_dir, 'a/alpha-1.2.3.tar.gz'),
                       finder.find_source('alpha', '1.2.3', 'linux') )
     
   def test_local_finder_tgz(self):
-    tmp_source_dir = temp_content.write_items_to_temp_dir([
-      'file a/alpha-1.2.3.tgz "%s" 644' % (self.TGZ_TARBALL),
-      'file b/beta-1.2.3.tgz "%s" 644' % (self.TGZ_TARBALL),
+    tmp_source_dir = source_dir_maker.make([
+      'file a/alpha-1.2.3.tgz "${tarball}" 644',
+      'file b/beta-1.2.3.tgz "${tarball}" 644',
     ])
     finder = local_source_finder(tmp_source_dir)
     self.assertEqual( path.join(tmp_source_dir, 'a/alpha-1.2.3.tgz'),
                       finder.find_source('alpha', '1.2.3', 'linux') )
 
   def test_local_finder_bz2(self):
-    tmp_source_dir = temp_content.write_items_to_temp_dir([
-      'file a/alpha-1.2.3.bz2 "%s" 644' % (self.BZ2_TARBALL),
-      'file b/beta-1.2.3.bz2 "%s" 644' % (self.BZ2_TARBALL),
+    tmp_source_dir = source_dir_maker.make([
+      'file a/alpha-1.2.3.bz2 "${tarball}" 644',
+      'file b/beta-1.2.3.bz2 "${tarball}" 644',
     ])
     finder = local_source_finder(tmp_source_dir)
     self.assertEqual( path.join(tmp_source_dir, 'a/alpha-1.2.3.bz2'),
                       finder.find_source('alpha', '1.2.3', 'linux') )
     
   def test_local_finder_zip(self):
-    tmp_source_dir = temp_content.write_items_to_temp_dir([
-      'file a/alpha-1.2.3.zip "%s" 644' % (self.ZIP_TARBALL),
-      'file b/beta-1.2.3.zip "%s" 644' % (self.ZIP_TARBALL),
+    tmp_source_dir = source_dir_maker.make([
+      'file a/alpha-1.2.3.zip "${tarball}" 644',
+      'file b/beta-1.2.3.zip "${tarball}" 644',
     ])
     finder = local_source_finder(tmp_source_dir)
     self.assertEqual( path.join(tmp_source_dir, 'a/alpha-1.2.3.zip'),
                       finder.find_source('alpha', '1.2.3', 'linux') )
     
   def test_local_finder_multiple_versions(self):
-    tmp_source_dir = temp_content.write_items_to_temp_dir([
-      'file a/alpha-1.2.3.tar.gz "%s" 644' % (self.TAR_GZ_TARBALL),
-      'file a/alpha-1.2.4.tar.gz "%s" 644' % (self.TAR_GZ_TARBALL),
-      'file a/alpha-1.2.5.tar.gz "%s" 644' % (self.TAR_GZ_TARBALL),
+    tmp_source_dir = source_dir_maker.make([
+      'file a/alpha-1.2.3.tar.gz "${tarball}" 644',
+      'file a/alpha-1.2.4.tar.gz "${tarball}" 644',
+      'file a/alpha-1.2.5.tar.gz "${tarball}" 644',
     ])
     finder = local_source_finder(tmp_source_dir)
     self.assertEqual( path.join(tmp_source_dir, 'a/alpha-1.2.3.tar.gz'),
                       finder.find_source('alpha', '1.2.3', 'linux') )
 
   def xtest_local_finder_multiple_close_versions(self):
-    tmp_source_dir = temp_content.write_items_to_temp_dir([
-      'file a/alpha-1.2.3.tar.gz "%s" 644' % (self.TAR_GZ_TARBALL),
-      'file a/alpha-1.2.3.1.tar.gz "%s" 644' % (self.TAR_GZ_TARBALL),
+    tmp_source_dir = source_dir_maker.make([
+      'file a/alpha-1.2.3.tar.gz "${tarball}" 644',
+      'file a/alpha-1.2.3.1.tar.gz "${tarball}" 644',
     ])
     finder = local_source_finder(tmp_source_dir)
     self.assertEqual( path.join(tmp_source_dir, 'a/alpha-1.2.3.tar.gz'),
                       finder.find_source('alpha', '1.2.3', 'linux') )
     
   def test_local_finder_invalid_archive(self):
-    tmp_dir = temp_content.write_items_to_temp_dir([
+    tmp_source_dir = source_dir_maker.make([
       'file a/alpha-1.2.3.tar.gz "alpha-1.2.3.tar.gz" 644',
       'file b/beta-1.2.3.tar.gz "beta-1.2.3.tar.gz" 644',
     ])
-    finder = local_source_finder(tmp_dir)
+    finder = local_source_finder(tmp_source_dir)
     with self.assertRaises(RuntimeError) as context:
       finder.find_source('alpha', '1.2.3', 'linux')
     
