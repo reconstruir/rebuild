1aa30334f63c77e5065c6895d68c85159b4f26d4
Ramiro Estrugo <ramiro@fateware.com>
2017-10-16 09:21:00 -0700

    working unit test for local source finder.

--- lib/rebuild/source_finder/local_source_finder.py
+++ lib/rebuild/source_finder/local_source_finder.py
@@ -2,29 +2,30 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from .source_finder import source_finder
-from rebuild import TarballUtil, version as reb_version, build_target as reb_build_target
+from rebuild import TarballUtil
 from bes.common import check_type
+from bes.archive import archiver
 
 class local_source_finder(source_finder):
 
   def __init__(self, where):
     self.where = where
     
-  def find_source(self, name, version, build_target):
+  def find_source(self, name, version, system):
     check_type.check_string(name, 'name')
-    check_type.check(version, reb_version, 'reb_version')
-    check_type.check(build_target, reb_build_target, 'reb_build_target')
+    check_type.check_string(version, 'version')
+    check_type.check_string(system, 'system')
 
-    tarball = TarballUtil.find(self.where, name, version.upstream_version)
+    tarball = TarballUtil.find(self.where, name, version)
 
     # If we find more than one tarball it could be because there are 1 for each platform so
     # filter out only the ones that match the current build system target
     if len(tarball) > 1:
-      pattern = '/%s/' % (build_target.system)
+      pattern = '/%s/' % (system)
       tarball = [ t for t in tarball if pattern in t ]
       
     if len(tarball) > 1:
-      raise RuntimeError('Too many tarballs found for %s-%s: %s' % (name, version.upstream_version, ' '.join(tarball)))
+      raise RuntimeError('Too many tarballs found for %s-%s: %s' % (name, version, ' '.join(tarball)))
     if len(tarball) == 0:
       return None
 
--- lib/rebuild/source_finder/tests/test_local_source_finder.py
+++ lib/rebuild/source_finder/tests/test_local_source_finder.py
@@ -3,14 +3,38 @@
 
 from bes.testing.unit_test import unit_test
 from bes.fs.testing import temp_content
+from bes.fs import temp_file
+from bes.archive.temp_archive import temp_archive
+from rebuild.source_finder import local_source_finder
+import os.path as path
 
 class test_local_finder(unit_test):
 
+  def _make_tarball(ext):
+    return temp_archive.make_temp_archive([ temp_archive.Item('foo.txt', content = 'foo.txt\n') ], ext).filename
+  
+  BZ2_TARBALL = _make_tarball('bz2')
+  TAR_GZ_TARBALL = _make_tarball('tar.gz')
+  TGZ_TARBALL = _make_tarball('tgz')
+  ZIP_TARBALL = _make_tarball('zip')
+  
   def test_local_finder(self):
-    tmp_dir = temp_content.write_items_temp_dir([
+    tmp_source_dir = temp_content.write_items_to_temp_dir([
+      'file a/alpha-1.2.3.tar.gz "%s" 644' % (self.TAR_GZ_TARBALL),
+      'file b/beta-1.2.3.tar.gz "%s" 644' % (self.TAR_GZ_TARBALL),
+    ])
+    finder = local_source_finder(tmp_source_dir)
+    self.assertEqual( path.join(tmp_source_dir, 'a/alpha-1.2.3.tar.gz'),
+                      finder.find_source('alpha', '1.2.3', 'linux') )
+    
+  def test_local_finder_invalid_archive(self):
+    tmp_dir = temp_content.write_items_to_temp_dir([
       'file a/alpha-1.2.3.tar.gz "alpha-1.2.3.tar.gz" 644',
       'file b/beta-1.2.3.tar.gz "beta-1.2.3.tar.gz" 644',
     ])
-  
+    finder = local_source_finder(tmp_dir)
+    with self.assertRaises(RuntimeError) as context:
+      finder.find_source('alpha', '1.2.3', 'linux')
+    
 if __name__ == '__main__':
   unit_test.main()
