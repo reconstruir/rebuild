4a43dcf9d6243cd962fb8f0fb25838da1ec653c2
Ramiro Estrugo <ramiro@fateware.com>
2017-10-17 15:56:49 -0700

    working test of chain

---
+++ lib/rebuild/source_finder/tests/test_source_finder_chain.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.testing.unit_test import unit_test
+from rebuild.source_finder import source_finder_chain, hard_coded_source_finder
+
+from test_local_source_finder import source_dir_maker
+
+class test_source_finder_chain(unit_test):
+
+  def test_find_empty(self):
+    chain = source_finder_chain()
+    self.assertEqual( None, chain.find_source('alpha', '1.2.3', 'linux') )
+    
+  def test_find_one_finder(self):
+    tarball = source_dir_maker.make_tarball('tar.gz')
+    chain = source_finder_chain()
+    chain.add_finder(hard_coded_source_finder(tarball, 'alpha', '1.2.3'))
+    self.assertEqual( tarball, chain.find_source('alpha', '1.2.3', 'linux') )
+    
+  def xtest_find_two_finders(self):
+    chain = source_finder_chain()
+
+    tarball1 = source_dir_maker.make_tarball('tar.gz')
+    chain.add_finder(hard_coded_source_finder(tarball1, 'alpha', '1.2.3'))
+
+    tarball2 = source_dir_maker.make_tarball('zip')
+    chain.add_finder(hard_coded_source_finder(tarball2, 'beta', '6.6.6'))
+
+    self.assertEqual( tarball1, chain.find_source('alpha', '1.2.3', 'linux') )
+    self.assertEqual( tarball2, chain.find_source('beta', '6.6.6', 'linux') )
+    
+if __name__ == '__main__':
+  unit_test.main()
