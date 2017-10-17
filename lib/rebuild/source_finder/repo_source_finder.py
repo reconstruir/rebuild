a8cdead9d9585f63d170364b0726015c04dd6a85
Ramiro Estrugo <ramiro@fateware.com>
2017-10-16 23:59:17 -0700

    cleanup and add a update once test.

--- lib/rebuild/source_finder/repo_source_finder.py
+++ lib/rebuild/source_finder/repo_source_finder.py
@@ -8,27 +8,28 @@ import os.path as path
 
 class repo_source_finder(source_finder):
 
-  def __init__(self, root, address, no_network = False):
+  def __init__(self, root, address, no_network = False, update_only_once = False):
     self.repo = repo(root, address)
     self.no_network = no_network
-#    self.updated = False
+    self.update_only_once = update_only_once
+    self._updated = False
     
   def find_source(self, name, version, system):
-    print('find_source(root=%s, address=%s, name=%s, version=%s, system=%s)' % (self.repo.root, self.repo.address, name, version, system))
-    self._update()
+    if self.update_only_once:
+      self._update_once()
+    else:
+      self._update()
     return self._find_tarball(self.repo.root, name, version, system)
 
-#  def update_once(self):
-#    if self.updated:
-#      return
-#    self.update()
-#    self.updated = True
+  def _update_once(self):
+    if self._updated:
+      return
+    self._update()
+    self._updated = True
 
   def _update(self):
-    print('_update(root=%s, address=%s)' % (self.repo.root, self.repo.address))
     if self.no_network:
-      build_blurb.blurb('build', 'Repo sources disabled due to no_network: %s' % (path.relpath(self.repo.root)))
+      build_blurb.blurb('build', 'Repo source finder update disabled due to no_network: %s' % (path.relpath(self.repo.root)))
     else:
       build_blurb.blurb('build', 'Updating repo sources: %s' % (path.relpath(self.repo.root)))
       self.repo.clone_or_pull()
-    
--- lib/rebuild/source_finder/tests/test_repo_source_finder.py
+++ lib/rebuild/source_finder/tests/test_repo_source_finder.py
@@ -15,30 +15,30 @@ class test_repo_source_finder(unit_test):
   _DEBUG = True
   
   def test_repo_source_finder(self):
-    tmp_source_repo_dir = self._make_git_repo([
+    tmp_source_repo = self._make_git_repo([
       'file a/alpha-1.2.3.tar.gz "${tarball}" 644',
       'file b/beta-1.2.3.tar.gz "${tarball}" 644',
     ], delete = not self._DEBUG)
     tmp_repo_dir = temp_file.make_temp_dir(delete = not self._DEBUG)
     if self._DEBUG:
-      print('tmp_source_repo_dir: %s' % (tmp_source_repo_dir))
+      print('tmp_source_repo: %s' % (tmp_source_repo.root))
       print('       tmp_repo_dir: %s' % (tmp_repo_dir))
-    finder = repo_source_finder(tmp_repo_dir, tmp_source_repo_dir)
+    finder = repo_source_finder(tmp_repo_dir, tmp_source_repo.root)
     self.assertEqual( path.join(tmp_repo_dir, 'a/alpha-1.2.3.tar.gz'),
                       finder.find_source('alpha', '1.2.3', 'linux') )
     self.assertEqual( [ 'a/alpha-1.2.3.tar.gz', 'b/beta-1.2.3.tar.gz' ],
                       finder.repo.find_all_files() )
 
   def test_repo_source_finder_update(self):
-    tmp_source_repo_dir = self._make_git_repo([
+    tmp_source_repo = self._make_git_repo([
       'file a/alpha-1.2.3.tar.gz "${tarball}" 644',
       'file b/beta-1.2.3.tar.gz "${tarball}" 644',
     ], delete = not self._DEBUG)
     tmp_repo_dir = temp_file.make_temp_dir(delete = not self._DEBUG)
     if self._DEBUG:
-      print('tmp_source_repo_dir: %s' % (tmp_source_repo_dir))
+      print('tmp_source_repo: %s' % (tmp_source_repo.root))
       print('       tmp_repo_dir: %s' % (tmp_repo_dir))
-    f1 = repo_source_finder(tmp_repo_dir, tmp_source_repo_dir)
+    f1 = repo_source_finder(tmp_repo_dir, tmp_source_repo.root)
     self.assertEqual( path.join(tmp_repo_dir, 'a/alpha-1.2.3.tar.gz'),
                       f1.find_source('alpha', '1.2.3', 'linux') )
     self.assertEqual( [ 'a/alpha-1.2.3.tar.gz', 'b/beta-1.2.3.tar.gz' ],
@@ -46,16 +46,39 @@ class test_repo_source_finder(unit_test):
 
     # Now add one file to the source repo
     d = source_dir_maker.make(['file g/gamma-1.2.3.tar.gz "${tarball}" 644'],
-                              delete = False, where = tmp_source_repo_dir)
-    self.assertEqual( tmp_source_repo_dir, d )
-    r = repo(tmp_source_repo_dir)
-    r.add('g/gamma-1.2.3.tar.gz')
-    r.commit('add', 'g/gamma-1.2.3.tar.gz')
+                              delete = False, where = tmp_source_repo.root)
+    self.assertEqual( tmp_source_repo.root, d )
+    tmp_source_repo.add('g/gamma-1.2.3.tar.gz')
+    tmp_source_repo.commit('add', 'g/gamma-1.2.3.tar.gz')
     self.assertEqual( path.join(tmp_repo_dir, 'g/gamma-1.2.3.tar.gz'),
                       f1.find_source('gamma', '1.2.3', 'linux') )
     self.assertEqual( [ 'a/alpha-1.2.3.tar.gz', 'b/beta-1.2.3.tar.gz', 'g/gamma-1.2.3.tar.gz' ],
                       f1.repo.find_all_files() )
 
+  def test_repo_source_finder_update_once(self):
+    tmp_source_repo = self._make_git_repo([
+      'file a/alpha-1.2.3.tar.gz "${tarball}" 644',
+      'file b/beta-1.2.3.tar.gz "${tarball}" 644',
+    ], delete = not self._DEBUG)
+    tmp_repo_dir = temp_file.make_temp_dir(delete = not self._DEBUG)
+    if self._DEBUG:
+      print('tmp_source_repo: %s' % (tmp_source_repo.root))
+      print('       tmp_repo_dir: %s' % (tmp_repo_dir))
+    f1 = repo_source_finder(tmp_repo_dir, tmp_source_repo.root, update_only_once = True)
+    self.assertEqual( path.join(tmp_repo_dir, 'a/alpha-1.2.3.tar.gz'),
+                      f1.find_source('alpha', '1.2.3', 'linux') )
+    self.assertEqual( [ 'a/alpha-1.2.3.tar.gz', 'b/beta-1.2.3.tar.gz' ],
+                      f1.repo.find_all_files() )
+
+    # Now add one file to the source repo
+    d = source_dir_maker.make(['file g/gamma-1.2.3.tar.gz "${tarball}" 644'],
+                              delete = False, where = tmp_source_repo.root)
+    self.assertEqual( tmp_source_repo.root, d )
+    tmp_source_repo.add('g/gamma-1.2.3.tar.gz')
+    tmp_source_repo.commit('add', 'g/gamma-1.2.3.tar.gz')
+    self.assertEqual( None, f1.find_source('gamma', '1.2.3', 'linux') )
+    self.assertEqual( [ 'a/alpha-1.2.3.tar.gz', 'b/beta-1.2.3.tar.gz' ],
+                      f1.repo.find_all_files() )
 
   @classmethod
   def _make_git_repo(clazz, items, delete = True):
@@ -64,7 +87,7 @@ class test_repo_source_finder(unit_test):
     r.init()
     r.add('.')
     r.commit('add', '.')
-    return r.root
+    return r
     
 if __name__ == '__main__':
   unit_test.main()
