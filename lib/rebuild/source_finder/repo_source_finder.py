b3ac6ec51bffa688fd3c7609f8ce02ffb8d42ecb
Ramiro Estrugo <ramiro@fateware.com>
2017-10-16 18:22:20 -0700

    test for repo source finder works

--- lib/rebuild/source_finder/repo_source_finder.py
+++ lib/rebuild/source_finder/repo_source_finder.py
@@ -3,10 +3,12 @@
 
 from .source_finder import source_finder
 from bes.git import repo
+from rebuild import build_blurb
+from os import path
 
 class repo_source_finder(source_finder):
 
-  def __init__(self, root, address, no_network):
+  def __init__(self, root, address, no_network = False):
     self.repo = repo(root, address)
     self.no_network = no_network
   
@@ -20,7 +22,7 @@ class repo_source_finder(source_finder):
       return
     self._UPDATED = True
     if self.no_network:
-      self.blurb('Repo sources disabled due to no_network: %s' % (path.relpath(self.repo.root)))
+      pass #build_blurb('build', 'Repo sources disabled due to no_network: %s' % (path.relpath(self.repo.root)))
     else:
-      self.blurb('Updating repo sources: %s' % (path.relpath(self.repo.root)))
+      #build_blurb('build', 'Updating repo sources: %s' % (path.relpath(self.repo.root)))
       self.repo.clone_or_pull()
