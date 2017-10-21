4c3a0af803e6501220ae12fd32341c1e937cb407
Ramiro Estrugo <ramiro@fateware.com>
2017-10-21 12:05:29 -0700

    strip works on linux

--- lib/rebuild/toolchain/strip.py
+++ lib/rebuild/toolchain/strip.py
@@ -2,7 +2,12 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from bes.system import impl_import
+from rebuild import binary_detector
 
 class strip(impl_import.load(__name__, '_strip', globals())):
   'Foo.'
-  pass
+
+  @classmethod
+  def check_strippable(clazz, binary):
+    if not binary_detector.is_strippable(binary):
+      raise RuntimeError('not a strippable binary: %s' % (binary))
