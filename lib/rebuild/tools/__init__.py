4fd7bf091cd128e228061abc88db82d8844910e4
Ramiro Estrugo <ramiro@fateware.com>
2017-11-04 20:22:39 -0700

    add

---
+++ lib/rebuild/tools/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from .install import install
+from .patch import patch
+from .archive_util import archive_util
