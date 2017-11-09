b4d64a3ec2ac515f0bff1ed2fb0ed5b0fbd9827d
Ramiro Estrugo <ramiro@fateware.com>
2017-11-09 01:54:03 -0800

    add

---
+++ lib/rebuild/packager/package_resolver.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+#import copy, fnmatch, os, os.path as path
+#from bes.common import algorithm, dict_util, object_util
+#from bes.thread import thread_pool
+#from bes.fs import dir_util, file_util
+#from collections import namedtuple
+#from rebuild.step_manager import step_aborted
+#
+#from rebuild.base import build_target
+
+from bes.common import check_type
+
+class package_resolver(object):
+
+  def __init__(self, script_manager):
+    self.script_manager = script_manager
+    
+  def resolve_packages(self, names, build_target):
+    check_type.check_string_list(names)
+    check_type.build_target(build_target)
+    return self.script_manager.resolve_requirements(names, build_target.system)
+
+  def package_names(self):
+    return self.script_manager.package_names()
+
+  def dependency_map(clazz, system):
+    'Return a map of requirements dependencies.  A dictionary keyed on name pointing  to a set of dependencies.'
+    assert isinstance(scripts, dict)
+    dep_map = {}
+    for name in sorted(scripts.keys()):
+      script = scripts[name]
+      requirements_names = script.descriptor.requirements_names_for_system(system)
+      build_requirements_names = script.descriptor.build_requirements_names_for_system(system)
+      dep_map[name] = requirements_names | build_requirements_names
+    return dep_map
---
+++ lib/rebuild/packager/tests/test_package_resolver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.testing.unit_test import unit_test
+#import os.path as path
+#from bes.fs import temp_file
+#from rebuild.package_manager import package_descriptor
+#from rebuild.base import build_category, build_target, requirement
+from rebuild.packager import package_resolver
+
+class test_package_resolver(unit_test):
+
+  def test_caca(self):
+    pass
+
+if __name__ == '__main__':
+  unit_test.main()
