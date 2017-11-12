e9f6063cf05352c656741e5dddadcb0591ade271
Ramiro Estrugo <ramiro@fateware.com>
2017-11-11 16:58:51 -0800

    wip

--- lib/rebuild/recipe/recipe_step_value_key.py
+++ lib/rebuild/recipe/recipe_step_value_key.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-#from collections import namedtuple
-#
-#from bes.compat import StringIO
-#from bes.key_value import key_value_parser
-#from bes.system import log
-#from bes.text import tree_text_parser
-#
-#from rebuild.base import build_version, masked_config, requirement
-#from rebuild.package_manager import package_descriptor
-#from rebuild.step_manager import step_description
-#
-
 from collections import namedtuple
-from bes.common import check_type
+from bes.common import bool_util, check_type
+from rebuild.step_manager import step_argspec
 
 class recipe_step_value_key(namedtuple('recipe_step_value_key', 'key,value')):
 
@@ -22,13 +11,39 @@ class recipe_step_value_key(namedtuple('recipe_step_value_key', 'key,value')):
     return clazz.__bases__[0].__new__(clazz, key, value)
 
   @classmethod
-  def parse(clazz, text):
+  def parse(clazz, text, argspec):
     check_type.check_string(text, 'text')
-    left, delimiter, right = text.partition(':')
-    left = left.strip()
-    if not left:
+    check_type.check_step_argspec(argspec, 'argspec')
+    text = clazz._strip_comment(text)
+    key, delimiter, value = text.partition(':')
+    key = key.strip()
+    if not key:
       raise ValueError('Invalid step value key: \"%s\"' % (text))
     if not delimiter:
-      return clazz(left, None)
-    right = right.strip() or None
-    return clazz(left, right)
+      return clazz(key, None)
+    value = value.strip() or None
+    if not value:
+      return clazz(key, value)
+    value = clazz._parse_value(value, argspec)
+    return clazz(key, value)
+  
+  @classmethod
+  def _parse_value(clazz, value, argspec):
+    if argspec == step_argspec.BOOL:
+      return bool_util.parse_bool(value)
+    elif argspec == step_argspec.INT:
+      return int(value)
+    elif argspec == step_argspec.MASKED_KEY_VALUES:
+      assert False
+    elif argspec == step_argspec.MASKED_STRING_LIST:
+      assert False
+    elif argspec == step_argspec.STRING:
+      return value
+    assert False
+    
+  @classmethod
+  def _strip_comment(clazz, s):
+    i = s.find('#')
+    if i >= 0:
+      return s[0:i]
+    return s
--- lib/rebuild/recipe/tests/test_recipe_step_value_key.py
+++ lib/rebuild/recipe/tests/test_recipe_step_value_key.py
@@ -3,17 +3,38 @@
 
 from bes.testing.unit_test import unit_test
 from rebuild.recipe import recipe_step_value_key
+from rebuild.step_manager import step_argspec
 
 class test_recipe_step_value_key(unit_test):
 
-  def test_left_only(self):
-    self.assertTrue( ( 'foo', None ), recipe_step_value_key.parse('foo') )
+  def test_key_only(self):
+    self.assertEqual( ( 'foo', None ), recipe_step_value_key.parse('foo', step_argspec.STRING) )
+    
+  def test_key_and_value(self):
+    self.assertEqual( ( 'foo', 'bar' ), recipe_step_value_key.parse('foo:bar', step_argspec.STRING) )
       
-  def test_left_and_right(self):
-    self.assertTrue( ( 'foo', 'bar' ), recipe_step_value_key.parse('foo:bar') )
-      
-  def test_left_and_empty_right(self):
-    self.assertTrue( ( 'foo', None ), recipe_step_value_key.parse('foo:') )
+  def test_key_and_empty_value(self):
+    self.assertEqual( ( 'foo', None ), recipe_step_value_key.parse('foo:', step_argspec.STRING) )
+    
+  def test_key_with_comment(self):
+    self.assertEqual( ( 'foo', None ), recipe_step_value_key.parse('foo  # comment', step_argspec.STRING) )
+    
+  def test_key_and_value_with_comment(self):
+    self.assertEqual( ( 'foo', 'bar' ), recipe_step_value_key.parse('foo:bar  # comment', step_argspec.STRING) )
+    
+  def test_key_and_empty_value_with_comment(self):
+    self.assertEqual( ( 'foo', None ), recipe_step_value_key.parse('foo:# comment', step_argspec.STRING) )
+    
+  def test_value_string(self):
+    self.assertEqual( ( 'foo', 'bar' ), recipe_step_value_key.parse('foo:bar', step_argspec.STRING) )
+
+  def test_value_bool(self):
+    self.assertEqual( ( 'foo', True ), recipe_step_value_key.parse('foo:True', step_argspec.BOOL) )
+    self.assertEqual( ( 'foo', True ), recipe_step_value_key.parse('foo:true', step_argspec.BOOL) )
+    self.assertEqual( ( 'foo', True ), recipe_step_value_key.parse('foo:1', step_argspec.BOOL) )
+    self.assertEqual( ( 'foo', False ), recipe_step_value_key.parse('foo:False', step_argspec.BOOL) )
+    self.assertEqual( ( 'foo', False ), recipe_step_value_key.parse('foo:false', step_argspec.BOOL) )
+    self.assertEqual( ( 'foo', False ), recipe_step_value_key.parse('foo:0', step_argspec.BOOL) )
     
 if __name__ == '__main__':
   unit_test.main()
