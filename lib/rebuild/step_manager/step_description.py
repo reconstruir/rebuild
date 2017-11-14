c1a06b1446d3cb31f8c4b34cfa68d7dd921674e9
Ramiro Estrugo <ramiro@fateware.com>
2017-11-13 16:07:06 -0800

    add argspec.  add parse_description

--- lib/rebuild/step_manager/step_description.py
+++ lib/rebuild/step_manager/step_description.py
@@ -2,18 +2,19 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 import copy
-from bes.common import object_util, string_util
+from bes.common import check_type, object_util, string_util
 from .step_registry import step_register, step_registry
 
 class step_description(object):
 
-  def __init__(self, step_class, args = {}):
+  def __init__(self, step_class, args = {}, argspec = None):
     assert isinstance(args, dict)
     self.step_class = step_class
     self.args = args
+    self.argspec = argspec or {}
 
   def __str__(self):
-    return '%s:%s' % (str(self.step_class), str(self.args))
+    return '%s:%s:%s' % (str(self.step_class), str(self.args), self.argspec)
 
   def __eq__(self, other):
     return self.__dict__ == other.__dict__
@@ -30,13 +31,22 @@ class step_description(object):
         step_class = step_registry.get(item)
         if not step_class:
           raise RuntimeError('no such step class: %s' % (item))
-        current_desc = step_description(step_class, {})
+        current_desc = step_description(step_class.clazz, args = {}, argspec = step_class.argspec)
         result.append(current_desc)
       else:
         assert current_desc
         assert isinstance(item, dict)
         current_desc.args.update(copy.deepcopy(item))
     return result
+
+  @classmethod
+  def parse_description(self, text):
+    check_type.check_string(text, 'text')
+    step_class = step_registry.get(text)
+    if not step_class:
+      raise RuntimeError('no such step class: %s' % (text))
+    desc = step_description(step_class.clazz, args = {}, argspec = step_class.argspec)
+    return desc
 
   @classmethod
   def is_step_description(clazz, o):
