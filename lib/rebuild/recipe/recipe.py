4473d548b620a4f5f19ea562da0f244f8f47511b
Ramiro Estrugo <ramiro@fateware.com>
2017-12-30 00:49:31 -0800

    more better to_string()

--- lib/rebuild/recipe/recipe.py
+++ lib/rebuild/recipe/recipe.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
+import re
 from collections import namedtuple
 from bes.common import node
 
 class recipe(namedtuple('recipe', 'format_version,filename,enabled,properties,requirements,build_requirements,descriptor,instructions,steps,load')):
 
+  CHECK_UNKNOWN_PROPERTIES = True
+  
   def __new__(clazz, format_version, filename, enabled, properties, requirements,
               build_requirements, descriptor, instructions, steps, load):
     return clazz.__bases__[0].__new__(clazz, format_version, filename, enabled, properties, requirements,
                                       build_requirements, descriptor, instructions, steps, load)
 
   def __str__(self):
-    return str(self._to_node()).strip()
+    return self.to_string()
+
+  def to_string(self, depth = 0, indent = 2):
+    s = self._to_node().to_string(depth = depth, indent = indent).strip()
+    lines = s.split('\n')
+    for i, line in enumerate(lines):
+      if line.isspace():
+        lines[i] = ''
+    return '\n'.join(lines)
   
   def _to_node(self):
     'A convenient way to make a recipe string is to build a graph first.'
@@ -35,18 +46,44 @@ class recipe(namedtuple('recipe', 'format_version,filename,enabled,properties,re
     return root
 
   @classmethod
-  def _properties_to_node(clazz, properties):
-    result = node('properties')
-    for key, value in sorted(properties.items()):
-      result.add_child('%s=%s' % (key, value))
-    return result
-
-  @classmethod
   def _requirements_to_node(clazz, label, requirements):
     result = node(label)
     for req in requirements:
       result.add_child(req.to_string_colon_format())
     return result
+  
+  @classmethod
+  def _properties_to_node(clazz, properties):
+    properties_node = node('properties')
+    for key in sorted([ key for key in properties.keys()]):
+      clazz._property_to_node(properties_node, key, properties)
+    return properties_node
+
+  @classmethod
+  def _property_to_node(clazz, properties_node, key, properties):
+    assert isinstance(properties_node, node)
+    assert key in properties
+    value = properties[key]
+    if key == 'export_compilation_flags_requirements':
+      properties_node.children.append(clazz._export_compilation_flags_requirements_to_node(properties))
+    elif key in [ 'category' ]:
+      properties_node.children.append(node('%s=%s' % (key, value)))
+    else:
+      if clazz.CHECK_UNKNOWN_PROPERTIES:
+        raise RuntimeError('Unknown property: %s' % (key))
+      else:
+        properties_node.children.append(node('%s=%s' % (key, value)))
+    del properties[key]
+
+  @classmethod
+  def _export_compilation_flags_requirements_to_node(clazz, properties):
+    assert 'export_compilation_flags_requirements' in properties
+    value = properties['export_compilation_flags_requirements']
+    assert isinstance(value, list)
+    child = node('export_compilation_flags_requirements')
+    for i in value:
+      child.add_child(i)
+    return child
 
   @classmethod
   def _steps_to_node(clazz, steps):
