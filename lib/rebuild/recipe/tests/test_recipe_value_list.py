dd5f5f01aa4ed25197916a7b34f33f519017b30e
Ramiro Estrugo <ramiro@fateware.com>
2018-01-02 13:39:56 -0800

    remove the hack to parse steps to test recipe value list and use a defined list instead

--- lib/rebuild/recipe/tests/test_recipe_value_list.py
+++ lib/rebuild/recipe/tests/test_recipe_value_list.py
@@ -2,76 +2,23 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from bes.testing.unit_test import unit_test
-from rebuild.recipe import recipe_parser
-from bes.key_value import key_value as KV, key_value_list as KVL
-from test_steps import *
+from rebuild.recipe import recipe_value as RV, recipe_value_list as RVL, masked_value as MV, masked_value_list as MVL
+from bes.key_value import key_value_list as KVL
 
 class test_recipe_value_list(unit_test):
 
-  def test_takes_all(self):
-    text = '''\
-bool_value:
-  all: True
-
-string_list_value
-  all: a b "x y"
-
-key_values_value
-  all: a=forall b=6
-  linux: a=forlinux
-  macos: a=formacos
-  android: a=forandroid
-'''
-    values = self._parse(text)
-    r = values.resolve('linux')
-    self.assertEqual( { 'key_values_value': KVL([ ( 'a', 'forlinux' ), ( 'b', '6' ) ]),
-                        'string_list_value': ['a', 'b', '"x y"'],
-                        'bool_value': True }, r )
-    r = values.resolve('macos')
-    self.assertEqual( { 'key_values_value': KVL([ ( 'a', 'formacos' ), ( 'b', '6' ) ]),
-                        'string_list_value': ['a', 'b', '"x y"'],
-                        'bool_value': True }, r )
-    r = values.resolve('android')
-    self.assertEqual( { 'key_values_value': KVL([ ( 'a', 'forandroid' ), ( 'b', '6' ) ]),
-                        'string_list_value': ['a', 'b', '"x y"'],
-                        'bool_value': True }, r )
-
-  @classmethod
-  def _parse(clazz, s):
-    recipe_template = '''!rebuildrecipe!
-package foo-1.2.3-4
-  steps
-    step_takes_all
-%s
-'''
-    indented_values = clazz._add_indent(s, 3)
-    recipe_text = recipe_template % (indented_values)
-    r = recipe_parser(recipe_text, '<test>').parse()
-    return r[0].steps[0].values
-    
-  @classmethod
-  def _add_indent(clazz, s, n):
-    indent = ' ' * 2 * n
-    lines = s.split('\n')
-    lines = [ '%s%s' % (indent, x) for x in lines ]
-    return '\n'.join(lines)
-
   def test_resolve(self):
-
-    text = '''\
-bool_value:
-  all: True
-
-string_list_value
-  all: a b "x y"
-
-key_values_value
-  all: a=forall b=6
-  linux: a=forlinux
-  macos: a=formacos
-  android: a=forandroid
-'''
-    values = self._parse(text)
+    values = RVL([
+      RV('bool_value', [ MV('all', True) ]),
+      RV('string_list_value', [ MV('all', [ 'a', 'b', '"x y"' ]) ]),
+      RV('key_values_value', [
+        MV('all', KVL([ ('a', 'forall'), ('b', '6') ])),
+        MV('linux', KVL([ ('a', 'forlinux') ])),
+        MV('macos', KVL([ ('a', 'formacos') ])),
+        MV('android', KVL([ ('a', 'forandroid') ])),
+        ]),
+    ])
+    
     r = values.resolve('linux')
     self.assertEqual( { 'key_values_value': KVL([ ( 'a', 'forlinux' ), ( 'b', '6' ) ]),
                         'string_list_value': ['a', 'b', '"x y"'],
