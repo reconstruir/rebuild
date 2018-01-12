617fb6e20380334dc32c0daccf2e98dea5628b7d
Ramiro Estrugo <ramiro@fateware.com>
2018-01-12 13:18:00 -0800

    print env works

--- lib/rebuild/test_data/packager/template/template-1.0.0/programs/template_print_env/template_print_env_main.c
+++ lib/rebuild/test_data/packager/template/template-1.0.0/programs/template_print_env/template_print_env_main.c
@@ -1,8 +1,13 @@
 #include <config.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
 
 int main()
 {
-  printf("%s\n", PACKAGE_STRING);
+  for(unsigned int i = 0; environ[i] != NULL; i++) {
+    printf("%s\n", environ[i]);
+  }
   return 0;
 }
