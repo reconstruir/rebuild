f45562fb7235c696bea9472c5c2383a88712b307
Ramiro Estrugo <ramiro@fateware.com>
2018-01-23 15:22:02 -0800

    new pkg config entry

---
+++ lib/rebuild/pkg_config/caca_entry.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.common import check, variable
+from bes.key_value import key_value
+from collections import namedtuple
+from .entry_type import entry_type
+
+class caca_entry(namedtuple('caca_entry', 'line,etype,value')):
+  'A single pkg-config .pc file entry.'
+
+  COLON = ':'
+  EQUAL = '='
+
+  def __new__(clazz, line, etype, value):
+    check.check_line_token(line)
+    check.check_entry_type(etype)
+    if value:
+      check.check_key_value(value)
+    return clazz.__bases__[0].__new__(clazz, line, etype, value)
+  
+  def __str__(self):
+    return self.line.text
+
+  def __repr__(self):
+    return str(self)
+  
+  @classmethod
+  def parse(clazz, line):
+    'Parse a pkg-config .pc file entry.'
+    check.check_line_token(line)
+    delimiter = clazz._first_delimiter(line.text)
+    value = None
+    etype = None
+    if delimiter == clazz.COLON:
+      value = clazz._parse_value(line, delimiter)
+      etype = entry_type.PROPERTY
+    elif delimiter == clazz.EQUAL:
+      value = clazz._parse_value(line, delimiter)
+      etype = entry_type.VARIABLE
+    elif not line.text.strip():
+      etype = entry_type.BLANK
+    else:
+      etype = entry_type.COMMENT
+    assert etype != None
+    return clazz(line, etype, value)
+
+  def is_variable(self):
+    return self.etype == entry_type.VARIABLE
+
+  def is_property(self):
+    return self.etype == entry_type.PROPERTY
+
+#  def replace(self, replacements):
+#    self.value = variable.substitute(self.value, replacements)
+
+  @classmethod
+  def _first_delimiter(clazz, s):
+    'Return which delimiter comes first in s (either : or =).'
+    colon_index = s.find(clazz.COLON)
+    equal_index = s.find(clazz.EQUAL)
+    colon_found = colon_index >= 0
+    equal_found = equal_index >= 0
+    if colon_found and equal_found:
+      if colon_index < equal_index:
+        return clazz.COLON
+      else:
+        return clazz.EQUAL
+    elif colon_found:
+      return clazz.COLON
+    elif equal_found:
+      return clazz.EQUAL
+    else:
+      return None
+
+  @classmethod
+  def _parse_value(clazz, line, delimiter):
+    v = line.text_no_comments.partition(delimiter)
+    assert v[1] == delimiter
+    return key_value(v[0].strip(), v[2].strip())
---
+++ tests/rebuild.pkg_config/test_caca_entry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.testing.unit_test import unit_test
+from rebuild.pkg_config.caca_entry import caca_entry
+from rebuild.pkg_config.entry_type import entry_type
+from bes.text import line_token
+
+class test_caca_entry(unit_test):
+
+  def test_parse_type(self):
+    self.assertEqual( entry_type.VARIABLE, caca_entry.parse(line_token(1, 'prefix=/usr/foo')).etype )
+    self.assertEqual( entry_type.PROPERTY, caca_entry.parse(line_token(1, 'Name: foo')).etype )
+    self.assertEqual( entry_type.BLANK, caca_entry.parse(line_token(1, '     ')).etype )
+    self.assertEqual( entry_type.COMMENT, caca_entry.parse(line_token(1, '# comment foo')).etype )
+    self.assertEqual( entry_type.VARIABLE, caca_entry.parse(line_token(1, 'prefix=/usr/foo # with comment')).etype )
+    
+  def test_parse_value(self):
+    self.assertEqual( ( 'prefix', '/usr/foo' ), caca_entry.parse(line_token(1, 'prefix=/usr/foo')).value )
+    self.assertEqual( ( 'prefix', '/usr/foo' ), caca_entry.parse(line_token(1, 'prefix = /usr/foo')).value )
+    self.assertEqual( ( 'prefix', '/usr/foo' ), caca_entry.parse(line_token(1, 'prefix = /usr/foo ')).value )
+    self.assertEqual( ( 'prefix', '/usr/foo' ), caca_entry.parse(line_token(1, ' prefix = /usr/foo ')).value )
+    self.assertEqual( ( 'prefix', '/usr/foo' ), caca_entry.parse(line_token(1, 'prefix=/usr/foo # comment')).value )
+    
+if __name__ == "__main__":
+  unit_test.main()
