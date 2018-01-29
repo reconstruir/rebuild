2276b92be1cbbd27366b45584a94a0e659e04386
Ramiro Estrugo <ramiro@fateware.com>
2018-01-28 20:43:56 -0800

    print-requires kinda works

---
+++ tests/test_data/packager/template/code/orange/orange1.h
@@ -0,0 +1,7 @@
+#ifndef orange1_h__
+#define orange1_h__
+
+extern int orange1_foo(int x);
+extern int orange1_bar(int x);
+
+#endif /* orange1_h__ */
---
+++ tests/test_data/packager/template/code/orange/orange2.h
@@ -0,0 +1,9 @@
+#ifndef orange2_h__
+#define orange2_h__
+
+#include <orange1/orange1.h>
+
+extern int orange2_foo(int x);
+extern int orange2_bar(int x);
+
+#endif /* orange2_h__ */
