dc207ed64840a1b53e85b02f67b1c51cf8701337
Ramiro Estrugo <ramiro@fateware.com>
2018-02-18 02:23:32 -0800

    wip

--- lib/rebuild/__init__.py
+++ lib/rebuild/__init__.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.version import version
+
+__version__, __tag__ = version.version(__name__, 'version.txt')
+__author__ = 'bes@fateware.com'
