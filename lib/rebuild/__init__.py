04f356043839c5c92200a8d8933b9eef875e11f1
Ramiro Estrugo <ramiro@fateware.com>
2018-02-23 23:57:35 -0800

    foo

--- lib/rebuild/__init__.py
+++ lib/rebuild/__init__.py
@@ -3,6 +3,7 @@
 
 from .ver import BES_VERSION as __version__
 from .ver import BES_AUTHOR_EMAIL as __author__
+from .ver import BES_AUTHOR_NAME as __bes_author_name__
 from .ver import BES_ADDRESS as __bes_address__
 from .ver import BES_TAG as __bes_tag__
 from .ver import BES_TIMESTAMP as __bes_timestamp__
