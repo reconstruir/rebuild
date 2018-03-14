c316405db20fbebbba5028854d396e67458a83f6
Ramiro Estrugo <ramiro@fateware.com>
2018-03-13 20:56:49 -0700

    reimplement the module loading with pkgutil instead of globbing the local disk

--- lib/rebuild/builder/steps/__init__.py
+++ lib/rebuild/builder/steps/__init__.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-# Find any files in this directory that match the pattern step_*.py and import
-# everything from it
-import glob, os.path as path
-_this_dir = path.dirname(__file__)
-_step_files = glob.glob('%s/step_*.py' % (_this_dir))
-_step_files = [ path.basename(f) for f in _step_files ]
-_step_modules = [ f[0:-3] for f in _step_files ]
-for mod in _step_modules:
-  _source = 'from .%s import *' % (mod)
+import pkgutil, sys
+_steps_path = sys.modules['rebuild.builder.steps'].__path__
+for _, modname, _ in pkgutil.iter_modules(path = _steps_path):
+  _source = 'from .%s import *' % (modname)
   _code = compile(_source, __file__, 'exec')
   exec(_code, globals(), locals())
