ac1d2d68fdfadd28c9f5dbd8669db0fa4be6532a
Ramiro Estrugo <ramiro@fateware.com>
2018-03-18 01:25:55 -0700

    move to src

--- tests/test_data/rebuilder/basic/libs/libstarch/build.py
+++ tests/test_data/rebuilder/basic/libs/libstarch/build.py
@@ -2,21 +2,23 @@
 def rebuild_recipes(env):
 
   tests = [
-    'all: test-tfoo.sh',
+    'all: test-libstarch.c',
   ]
 
   return env.args(
     properties = env.args(
-      name = 'tfoo',
+      name = 'libstarch',
       version = '1.0.0',
-      category = 'tool',
+      category = 'lib',
     ),
     steps = [
       'step_setup', {
         'no_tarballs': True,
+        'copy_source_to_build_dir': True,
       },
+      'step_make',
+      'step_make_install',
       'step_post_install', {
-        'install_files': [ 'all: files/bin/tfoo.py bin/tfoo.py' ],
         'tests': tests,
         'skip_binary_third_party_prefix': True,
       }
--- tests/test_data/rebuilder/basic/libs/libstarch/test-libstarch.c
+++ tests/test_data/rebuilder/basic/libs/libstarch/test-libstarch.c
@@ -1,32 +1,14 @@
 #include <libstarch/amylose.h>
 #include <libstarch/amylopectin.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-int amylose_foo(int x)
-{
-  return 100;
-}
-
-int amylose_bar(int x)
-{
-  return 200;
-}
-
-int amylopectin_foo(int x)
-{
-  return 600;
-}
-
-int amylopectin_bar(int x)
-{
-  return 700;
-}
 
 int main()
 {
-  int x = amylose_foo(0) + amylose_bar(int x)
-    printf("%s\n", environ[i]);
+  int EXPECTED_X = 701;
+  int x = amylose_foo(0) + amylopectin_foo(0);
+  if (x != EXPECTED_X) {
+    fprintf(stderr, "error x is %d instead of %d", x, EXPECTED_X);
+    return 1;
   }
   return 0;
 }
