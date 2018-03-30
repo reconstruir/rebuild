70fb93b316df11430d0e7bc1f5d7aecc0dbc83eb
Ramiro Estrugo <ramiro@fateware.com>
2018-03-29 22:56:21 -0700

    make it work with the new style recipes

--- bin/rebuild_update_tarball_address.py
+++ bin/rebuild_update_tarball_address.py
@@ -5,21 +5,25 @@ import os.path as path
 from rebuild.builder.builder_recipe_loader import builder_recipe_loader
 from bes.git import repo
 from bes.fs import file_replace, temp_file
+from rebuild.base import build_system
 
 def main():
-  filename = path.abspath('build.py')
-  r = builder_recipe_loader.load(filename)
-  tarball_address = r[0].steps[0].args['tarball_address']
-  address = tarball_address[0]
-  tag = tarball_address[1]
-  gr = repo(temp_file.make_temp_dir(), address = address)
-  gr.clone()
-  last = gr.last_commit_hash(short_hash = True)
-  if last == tag:
-    return 0
-  replacements = { tag: last }
-  print('%s: update %s -> %s' % (filename, tag, last))
-  file_replace.replace(filename, replacements, backup = True, word_boundary = True)
+  filename = path.abspath('build.rebc')
+  recipes = builder_recipe_loader.load(filename)
+  for recipe in recipes:
+    values = recipe.steps[0].resolve_values(build_system.MACOS)
+    tarball_address = values.get('tarball_address')
+    if tarball_address:
+      tarball_address_address = tarball_address[0]
+      old_revision = tarball_address[1]
+      gr = repo(temp_file.make_temp_dir(), address = tarball_address_address)
+      gr.clone()
+      new_revision = gr.last_commit_hash(short_hash = True)
+      if old_revision == new_revision:
+        return 0
+      replacements = { old_revision: new_revision }
+      print('%s: update %s -> %s' % (filename, old_revision, new_revision))
+      file_replace.replace(filename, replacements, backup = True, word_boundary = True)
   return 0
 
 main()
