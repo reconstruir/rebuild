0c7f12c26142cf08c386339ab882810e39e2e88b
Ramiro Estrugo <ramiro@fateware.com>
2018-04-02 08:45:39 -0700

    new package metadata

--- lib/rebuild/package/package_metadata.py
+++ lib/rebuild/package/package_metadata.py
@@ -1,23 +1,157 @@
-#!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-#import re
+import json, pickle
 from collections import namedtuple
-#from bes.common import check, node
-#from bes.text import white_space
+from bes.common import check, string_util
+from rebuild.base import build_target, build_version, package_descriptor, requirement_list
 
-class package_metadata(namedtuple('package_metadata', 'name,version,system,distro,archs,level,requirements,build_tool_requirements,build_requirements,env_vars,pkg_config_name')):
+class package_metadata(namedtuple('package_metadata', 'format_version,filename,checksum,name,version,revision,epoch,system,level,archs,distro,requirements,properties,files')):
 
-  def __new__(clazz, name, version, system, distro, archs, level, requirements,
-              build_tool_requirements, build_requirements, env_vars, pkg_config_name):
-    if env_vars:
-      check.check_masked_value_list(env_vars)
-    return clazz.__bases__[0].__new__(name, version, system, distro, archs, level, requirements,
-                                      build_tool_requirements, build_requirements, env_vars, pkg_config_name)
+  def __new__(clazz, filename, checksum, name, version, revision, epoch, system, level, archs, distro, requirements, properties, files):
+    if filename:
+      check.check_string(filename)
+    if checksum:
+      check.check_string(checksum)
+    check.check_string(name)
+    check.check_string(version)
+    check.check_int(revision)
+    check.check_int(epoch)
+    check.check_string(system)
+    check.check_string(level)
+    check.check_string_seq(archs)
+    if distro:
+      check.check_string(distro)
+    check.check_requirement_list(requirements)
+    check.check_dict(properties)
+    check.check_string_seq(files)
+    return clazz.__bases__[0].__new__(clazz, 2, filename, checksum, name, version, revision, epoch, system, level, archs, distro, requirements, properties, files)
 
-#  def __str__(self):
-#    return self.to_string()
+  @property
+  def build_version(self):
+    return build_version(self.version, self.revision, self.epoch)
+  
+  @property
+  def descriptor(self):
+    return package_descriptor(self.name, str(self.build_version), properties = self.properties, requirements = self.requirements)
 
-#  def to_string(self, depth = 0, indent = 2):
-#    s = self._to_node().to_string(depth = depth, indent = indent).strip()
-#    return white_space.shorten_multi_line_spaces(s)
+  @property
+  def build_target(self):
+    return build_target(system = self.system, level = self.level, archs = self.archs, distro = self.distro)
+    
+  def to_json(self):
+    return json.dumps(self.to_simple_dict(), indent = 2, sort_keys = True)
+
+  @classmethod
+  def parse_json(clazz, text):
+    o = json.loads(text)
+    format_version = o.get('_format_version', 1)
+    if format_version == 1:
+      return clazz._parse_json_v1(o)
+    elif format_version == 2:
+      return clazz._parse_json_v2(o)
+    else:
+      raise ValueError('invalid format_version: %s' % (format_version))
+
+  @classmethod
+  def _parse_json_v1(clazz, o):
+    if 'descriptor' in o:
+      key = 'descriptor'
+    else:
+      key = 'info'
+    assert key in o
+    assert 'files' in o
+    dd = o[key]
+    check.check_dict(dd)
+    descriptor = package_descriptor.parse_dict(dd)
+    return clazz('',
+                 '',
+                 descriptor.name,
+                 descriptor.version.upstream_version,
+                 descriptor.version.revision,
+                 descriptor.version.epoch,
+                 '',
+                 '',
+                 '',
+                 None,
+                 descriptor.requirements,
+                 descriptor.properties,
+                 o['files'])
+  
+  @classmethod
+  def _parse_json_v2(clazz, o):
+    return clazz(o['filename'],
+                 o['checksum'],
+                 o['name'],
+                 o['version'],
+                 o['revision'],
+                 o['epoch'],
+                 o['system'],
+                 o['level'],
+                 o['archs'],
+                 o['distro'],
+                 clazz._parse_requirements(o['requirements']),
+                 o['properties'],
+                 o['files'])
+  
+  @classmethod
+  def _parse_requirements(clazz, l):
+    check.check_string_seq(l)
+    reqs = requirement_list()
+    for n in l:
+      reqs.extend(requirement_list.parse(n))
+    return reqs
+
+  def to_simple_dict(self):
+    return {
+      '_format_version': self.format_version,
+      'filename': self.filename,
+      'checksum': self.checksum,
+      'name': self.name,
+      'version': self.version,
+      'revision': self.revision,
+      'epoch': self.epoch,
+      'system': self.system,
+      'level': self.level,
+      'archs': self.archs,
+      'distro': self.distro,
+      'requirements': [ str(r) for r in self.requirements ],
+      'properties': self.properties,
+      'files': [ f for f in self.files ],
+    }
+  
+  def to_sql_dict(self):
+    d = {
+      'filename': self.filename,
+      'checksum': self.checksum,
+      'name': self.name,
+      'version': self.version,
+      'revision': self.revision,
+      'epoch': self.epoch,
+      'system': self.system,
+      'level': self.level,
+      'archs': ' '.join(self.archs),
+      'distro': self.distro,
+      'requirements': json.dumps([ str(r) for r in self.requirements ]),
+      'properties': json.dumps(self.properties),
+      'files': json.dumps([ f for f in self.files ]),
+    }
+    for key, value in d.iteritems():
+      if value is None:
+        d[key] = 'null'
+      elif check.is_string(value):
+        d[key] = string_util.quote(value, quote_char = '\'')
+      else:
+        d[key] = str(value)
+    return d
+
+  @classmethod
+  def from_sql_row(clazz, row):
+    row = list(row)
+    row[9] = row[9].split(' ')
+    row[11] = clazz._parse_requirements(json.loads(row[11]))
+    row[12] = json.loads(row[12])
+    row[13] = json.loads(row[13])
+    row.pop(0)
+    return clazz(*row)
+  
+check.register_class(package_metadata, include_seq = False)
--- tests/lib/rebuild/package/test_package_metadata.py
+++ tests/lib/rebuild/package/test_package_metadata.py
@@ -1,16 +1,113 @@
 #!/usr/bin/env python
-#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
-
-from bes.testing.unit_test import unit_test
+#-*- coding:utf-8 -*-
+#
 import os.path as path
-#from bes.fs import temp_file, temp_item
-from rebuild.package import package_metadata as PM
+from bes.testing.unit_test import unit_test
+from rebuild.base import build_system, build_target, package_descriptor, requirement_list as RL
+from rebuild.package.package_metadata import package_metadata as PM
 
 class test_package_metadata(unit_test):
 
+  TEST_REQUIREMENTS = RL.parse('foo >= 1.2.3-1 bar >= 6.6.6-1')
+  TEST_FILES = [ 'f1', 'f2' ]
+  TEST_PROPERTIES = { 'p1': 'v1', 'p2': 6 }
+
+  TEST_ENTRY = PM('kiwi-6.7.8-2.tar.gz', '', 'kiwi', '6.7.8', 2, 0, 'macos', 'release', [ 'x86_64' ], None,
+                  TEST_REQUIREMENTS, TEST_PROPERTIES, TEST_FILES)
+
+  def test_descriptor(self):
+    self.assertEqual( package_descriptor('kiwi', '6.7.8-2', self.TEST_PROPERTIES, self.TEST_REQUIREMENTS),
+                      self.TEST_ENTRY.descriptor )
+
+  def test_build_target(self):
+    self.assertEqual( build_target('macos', 'release', [ 'x86_64' ], None), self.TEST_ENTRY.build_target )
+
   def test_to_json(self):
-    pass
-    #a = PM('foo', '1.0.0', 'macos', [ 'x86_64' ], 'lib', 'release', [], [], [], 
+    self.maxDiff = None
+    expected_json = '''\
+{
+  "_format_version": 2, 
+  "archs": [
+    "x86_64"
+  ], 
+  "checksum": "", 
+  "distro": null, 
+  "epoch": 0, 
+  "filename": "kiwi-6.7.8-2.tar.gz", 
+  "files": [
+    "f1", 
+    "f2"
+  ], 
+  "level": "release", 
+  "name": "kiwi", 
+  "properties": {
+    "p1": "v1", 
+    "p2": 6
+  }, 
+  "requirements": [
+    "foo >= 1.2.3-1", 
+    "bar >= 6.6.6-1"
+  ], 
+  "revision": 2, 
+  "system": "macos", 
+  "version": "6.7.8"
+}'''
+    self.assertMultiLineEqual( expected_json, self.TEST_ENTRY.to_json() )
+
+  def test_parse_json_v2(self):
+    json = '''\
+{
+  "_format_version": 2, 
+  "archs": [
+    "x86_64"
+  ], 
+  "checksum": "", 
+  "distro": null, 
+  "epoch": 0, 
+  "filename": "kiwi-6.7.8-2.tar.gz", 
+  "files": [
+    "f1", 
+    "f2"
+  ], 
+  "level": "release", 
+  "name": "kiwi", 
+  "properties": {
+    "p1": "v1", 
+    "p2": 6
+  }, 
+  "requirements": [
+    "foo >= 1.2.3-1", 
+    "bar >= 6.6.6-1"
+  ], 
+  "revision": 2, 
+  "system": "macos", 
+  "version": "6.7.8"
+}'''
+    actual_entry = PM.parse_json(json)
+    self.maxDiff = None
+    self.assertEqual( self.TEST_ENTRY, actual_entry )
+
+  def test_parse_json_v1(self):
+    json = '''\
+{
+  "files": [
+    "f1", 
+    "f2"
+  ],
+  "descriptor": {
+    "version": "6.7.8-2", 
+    "name": "kiwi", 
+    "requirements": [
+      "foo >= 1.2.3-1", 
+      "bar >= 6.6.6-1"
+    ] ,
+  "properties": { "p1": "v1", "p2": 6 }
+  }
+}'''
+    expected_entry = PM('', '', 'kiwi', '6.7.8', 2, 0, '', '', '', None,
+                        self.TEST_REQUIREMENTS, self.TEST_PROPERTIES, self.TEST_FILES)
+    actual_entry = PM.parse_json(json)
+    self.assertEqual( expected_entry, actual_entry )
 
 if __name__ == '__main__':
   unit_test.main()
