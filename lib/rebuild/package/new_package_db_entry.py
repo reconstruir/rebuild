d7ee8b7a94116318489344537100252af73a7366
Ramiro Estrugo <ramiro@fateware.com>
2018-04-02 08:43:10 -0700

    works

--- lib/rebuild/package/new_package_db_entry.py
+++ lib/rebuild/package/new_package_db_entry.py
@@ -1,53 +1,45 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-import json
+import json, pickle
 from collections import namedtuple
-from bes.common import check
-from rebuild.base import build_target, package_descriptor, requirement_list
+from bes.common import check, string_util
+from rebuild.base import build_target, build_version, package_descriptor, requirement_list
 
-class new_package_db_entry(namedtuple('new_package_db_entry', 'format_version,filename,checksum,name,version,system,level,arch,distro,requirements,properties,files')):
+class new_package_db_entry(namedtuple('new_package_db_entry', 'format_version,filename,checksum,name,version,revision,epoch,system,level,archs,distro,requirements,properties,files')):
 
-  def __new__(clazz, filename, checksum, name, version, system, level, arch, distro, requirements, properties, files):
+  def __new__(clazz, filename, checksum, name, version, revision, epoch, system, level, archs, distro, requirements, properties, files):
     if filename:
       check.check_string(filename)
     if checksum:
       check.check_string(checksum)
     check.check_string(name)
     check.check_string(version)
+    check.check_int(revision)
+    check.check_int(epoch)
     check.check_string(system)
     check.check_string(level)
-    check.check_string(arch)
+    check.check_string_seq(archs)
     if distro:
       check.check_string(distro)
     check.check_requirement_list(requirements)
     check.check_dict(properties)
     check.check_string_seq(files)
-    return clazz.__bases__[0].__new__(clazz, 2, filename, checksum, name, version, system, level, arch, distro, requirements, properties, files)
+    return clazz.__bases__[0].__new__(clazz, 2, filename, checksum, name, version, revision, epoch, system, level, archs, distro, requirements, properties, files)
 
   @property
+  def build_version(self):
+    return build_version(self.version, self.revision, self.epoch)
+  
+  @property
   def descriptor(self):
-    return package_descriptor(self.name, self.version, properties = self.properties, requirements = self.requirements)
+    return package_descriptor(self.name, str(self.build_version), properties = self.properties, requirements = self.requirements)
 
   @property
   def build_target(self):
-    return build_target(system = self.system, level = self.level, archs = [ self.arch ], distro = self.distro)
+    return build_target(system = self.system, level = self.level, archs = self.archs, distro = self.distro)
     
   def to_json(self):
-    d = {
-      '_format_version': self.format_version,
-      'filename': self.filename,
-      'checksum': self.checksum,
-      'name': self.name,
-      'version': self.version,
-      'system': self.system,
-      'level': self.level,
-      'arch': self.arch,
-      'distro': self.distro,
-      'requirements': [ str(r) for r in self.requirements ],
-      'properties': self.properties,
-      'files': [ f for f in self.files ],
-    }
-    return json.dumps(d, indent = 2, sort_keys = True)
+    return json.dumps(self.to_simple_dict(), indent = 2, sort_keys = True)
 
   @classmethod
   def parse_json(clazz, text):
@@ -74,7 +66,9 @@ class new_package_db_entry(namedtuple('new_package_db_entry', 'format_version,fi
     return clazz('',
                  '',
                  descriptor.name,
-                 str(descriptor.version),
+                 descriptor.version.upstream_version,
+                 descriptor.version.revision,
+                 descriptor.version.epoch,
                  '',
                  '',
                  '',
@@ -89,14 +83,15 @@ class new_package_db_entry(namedtuple('new_package_db_entry', 'format_version,fi
                  o['checksum'],
                  o['name'],
                  o['version'],
+                 o['revision'],
+                 o['epoch'],
                  o['system'],
                  o['level'],
-                 o['arch'],
+                 o['archs'],
                  o['distro'],
                  clazz._parse_requirements(o['requirements']),
                  o['properties'],
                  o['files'])
-  
   
   @classmethod
   def _parse_requirements(clazz, l):
@@ -106,3 +101,57 @@ class new_package_db_entry(namedtuple('new_package_db_entry', 'format_version,fi
       reqs.extend(requirement_list.parse(n))
     return reqs
 
+  def to_simple_dict(self):
+    return {
+      '_format_version': self.format_version,
+      'filename': self.filename,
+      'checksum': self.checksum,
+      'name': self.name,
+      'version': self.version,
+      'revision': self.revision,
+      'epoch': self.epoch,
+      'system': self.system,
+      'level': self.level,
+      'archs': self.archs,
+      'distro': self.distro,
+      'requirements': [ str(r) for r in self.requirements ],
+      'properties': self.properties,
+      'files': [ f for f in self.files ],
+    }
+  
+  def to_sql_dict(self):
+    d = {
+      'filename': self.filename,
+      'checksum': self.checksum,
+      'name': self.name,
+      'version': self.version,
+      'revision': self.revision,
+      'epoch': self.epoch,
+      'system': self.system,
+      'level': self.level,
+      'archs': ' '.join(self.archs),
+      'distro': self.distro,
+      'requirements': json.dumps([ str(r) for r in self.requirements ]),
+      'properties': json.dumps(self.properties),
+      'files': json.dumps([ f for f in self.files ]),
+    }
+    for key, value in d.iteritems():
+      if value is None:
+        d[key] = 'null'
+      elif check.is_string(value):
+        d[key] = string_util.quote(value, quote_char = '\'')
+      else:
+        d[key] = str(value)
+    return d
+
+  @classmethod
+  def from_sql_row(clazz, row):
+    row = list(row)
+    row[9] = row[9].split(' ')
+    row[11] = clazz._parse_requirements(json.loads(row[11]))
+    row[12] = json.loads(row[12])
+    row[13] = json.loads(row[13])
+    row.pop(0)
+    return clazz(*row)
+  
+check.register_class(new_package_db_entry, include_seq = False)
--- tests/lib/rebuild/package/test_new_package_db_entry.py
+++ tests/lib/rebuild/package/test_new_package_db_entry.py
@@ -8,11 +8,11 @@ from rebuild.package.new_package_db_entry import new_package_db_entry as PE
 
 class test_new_package_db_entry(unit_test):
 
-  TEST_REQUIREMENTS = RL.parse('foo >= 1.2.3-1 bar >= 6.6.6-1', default_system_mask = build_system.ALL)
+  TEST_REQUIREMENTS = RL.parse('foo >= 1.2.3-1 bar >= 6.6.6-1')
   TEST_FILES = [ 'f1', 'f2' ]
   TEST_PROPERTIES = { 'p1': 'v1', 'p2': 6 }
 
-  TEST_ENTRY = PE('kiwi-6.7.8-2.tar.gz', '', 'kiwi', '6.7.8-2', 'macos', 'release', 'x86_64', None,
+  TEST_ENTRY = PE('kiwi-6.7.8-2.tar.gz', '', 'kiwi', '6.7.8', 2, 0, 'macos', 'release', [ 'x86_64' ], None,
                   TEST_REQUIREMENTS, TEST_PROPERTIES, TEST_FILES)
 
   def test_descriptor(self):
@@ -26,34 +26,65 @@ class test_new_package_db_entry(unit_test):
     self.maxDiff = None
     expected_json = '''\
 {
- "_format_version": 2,
- "arch": "x86_64",
- "checksum": "",
- "distro": null,
- "filename": "kiwi-6.7.8-2.tar.gz",
- "files": [ "f1", "f2" ],
- "level": "release",
- "name": "kiwi",
- "properties": { "p1": "v1", "p2": 6 },
- "requirements": [ "foo >= 1.2.3-1", "bar >= 6.6.6-1" ], "system": "macos", "version": "6.7.8-2"
+  "_format_version": 2, 
+  "archs": [
+    "x86_64"
+  ], 
+  "checksum": "", 
+  "distro": null, 
+  "epoch": 0, 
+  "filename": "kiwi-6.7.8-2.tar.gz", 
+  "files": [
+    "f1", 
+    "f2"
+  ], 
+  "level": "release", 
+  "name": "kiwi", 
+  "properties": {
+    "p1": "v1", 
+    "p2": 6
+  }, 
+  "requirements": [
+    "foo >= 1.2.3-1", 
+    "bar >= 6.6.6-1"
+  ], 
+  "revision": 2, 
+  "system": "macos", 
+  "version": "6.7.8"
 }'''
-    self.assert_string_equal_ws( expected_json, self.TEST_ENTRY.to_json() )
+    self.assertMultiLineEqual( expected_json, self.TEST_ENTRY.to_json() )
 
   def test_parse_json_v2(self):
     json = '''\
 {
- "_format_version": 2,
- "arch": "x86_64",
- "checksum": "",
- "distro": null,
- "filename": "kiwi-6.7.8-2.tar.gz",
- "files": [ "f1", "f2" ],
- "level": "release",
- "name": "kiwi",
- "properties": { "p1": "v1", "p2": 6 },
- "requirements": [ "all: foo >= 1.2.3-1", "all: bar >= 6.6.6-1" ], "system": "macos", "version": "6.7.8-2"
+  "_format_version": 2, 
+  "archs": [
+    "x86_64"
+  ], 
+  "checksum": "", 
+  "distro": null, 
+  "epoch": 0, 
+  "filename": "kiwi-6.7.8-2.tar.gz", 
+  "files": [
+    "f1", 
+    "f2"
+  ], 
+  "level": "release", 
+  "name": "kiwi", 
+  "properties": {
+    "p1": "v1", 
+    "p2": 6
+  }, 
+  "requirements": [
+    "foo >= 1.2.3-1", 
+    "bar >= 6.6.6-1"
+  ], 
+  "revision": 2, 
+  "system": "macos", 
+  "version": "6.7.8"
 }'''
     actual_entry = PE.parse_json(json)
+    self.maxDiff = None
     self.assertEqual( self.TEST_ENTRY, actual_entry )
 
   def test_parse_json_v1(self):
@@ -67,13 +98,13 @@ class test_new_package_db_entry(unit_test):
     "version": "6.7.8-2", 
     "name": "kiwi", 
     "requirements": [
-      "all: foo >= 1.2.3-1", 
-      "all: bar >= 6.6.6-1"
+      "foo >= 1.2.3-1", 
+      "bar >= 6.6.6-1"
     ] ,
   "properties": { "p1": "v1", "p2": 6 }
   }
 }'''
-    expected_entry = PE('', '', 'kiwi', '6.7.8-2', '', '', '', None,
+    expected_entry = PE('', '', 'kiwi', '6.7.8', 2, 0, '', '', '', None,
                         self.TEST_REQUIREMENTS, self.TEST_PROPERTIES, self.TEST_FILES)
     actual_entry = PE.parse_json(json)
     self.assertEqual( expected_entry, actual_entry )
