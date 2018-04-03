ccbfaddc149ed86e4d7de586768231bfa5357fe2
Ramiro Estrugo <ramiro@fateware.com>
2018-04-03 16:27:34 -0700

    wip

--- lib/rebuild/package/package_list.py
+++ lib/rebuild/package/package_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-from bes.common import check
+from bes.common import check, type_checked_list
+from rebuild.base import package_descriptor_list
 from .package import package
 
 class package_list(object):
@@ -27,3 +28,31 @@ class package_list(object):
     'Return a list of descriptors for the given list of packages.'
     check.check_package_seq(packages)
     return [ package.descriptor for package in packages ]
+
+class xpackage_list(type_checked_list):
+
+  def __init__(self, values = None):
+    super(xpackage_list, self).__init__(package, values = values)
+
+  def sort_by_descriptor(self):
+    self._values = sorted(self._values, key = lambda package: package.descriptor)
+    
+  def descriptors(self):
+    'Return the names for all the descriptors.'
+    return package_descriptor_list([ p.descriptor for p in self ])
+
+  def latest_versions(self):
+    'Return a list of only the lastest version of any package with multiple versions.'
+    latest = {}
+    for package in self:
+      name = package.metadata.name
+      if not name in result:
+        latest[name] = package
+      else:
+        if package.metadata.build_version > result[name].metadata.build_version:
+          latest[name] = package
+    result = clazz(result.values())
+    result.sort_by_descriptor()
+    return result
+  
+#check.register_class(package_list, include_seq = False)
