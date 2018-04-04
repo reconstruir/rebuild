48ddad80dfd15aa10f1c503f452b77889de06de4
Ramiro Estrugo <ramiro@fateware.com>
2018-04-04 16:04:47 -0700

    add no base dir option

--- lib/rebuild/builder/steps/step_extract_tarballs_to_stage_dir.py
+++ lib/rebuild/builder/steps/step_extract_tarballs_to_stage_dir.py
@@ -14,13 +14,19 @@ class step_extract_tarballs_to_stage_dir(step):
   def define_args(clazz):
     return '''
     tarballs_strip_common_base bool False
+    tarballs_no_base_dir bool False
     '''
     
   def execute(self, script, env, args):
     tarballs = args.get('tarballs', [])
     strip_common_base = args.get('tarballs_strip_common_base', False)
+    if args.get('tarballs_no_base_dir', False):
+      base_dir = None
+    else:
+      base_dir = script.descriptor.full_name
+      
     for tarball in tarballs:
       archiver.extract(tarball, script.stage_dir,
-                       base_dir = script.descriptor.full_name,
+                       base_dir = base_dir,
                        strip_common_base = strip_common_base)
     return step_result(True)
