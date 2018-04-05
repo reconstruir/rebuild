52ed6d0334c6a21617af5828b607b13b30881414
Ramiro Estrugo <ramiro@fateware.com>
2018-04-04 20:48:23 -0700

    retire

--- lib/rebuild/tools/__init__.py
+++ lib/rebuild/tools/__init__.py
@@ -1,6 +1,5 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from .archive_util import archive_util
-from .dmg import dmg
 from .install import install
 from .patch import patch
