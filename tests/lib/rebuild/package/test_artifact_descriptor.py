6a50394f95c9a490f69a2eeb4ccd8e3866b7cbbc
Ramiro Estrugo <ramiro@fateware.com>
2018-04-08 22:46:51 -0700

    foo

--- tests/lib/rebuild/package/test_artifact_descriptor.py
+++ tests/lib/rebuild/package/test_artifact_descriptor.py
@@ -9,8 +9,18 @@ class test_artifact_descriptor(unit_test):
 
   def test_parse_filename(self):
     f = AD.parse_artifact_filename
+
     self.assertEqual( ( 'gnu_libtool', '2.4.6', 0, 0, 'macos', 'release', [ 'x86_64' ], None ),
                       f('macos/x86_64/release/gnu_libtool-2.4.6.tar.gz') )
+
+    self.assertEqual( ( 'gnu_libtool', '2.4.6', 0, 0, 'macos', 'debug', [ 'x86_64' ], None ),
+                      f('macos/x86_64/debug/gnu_libtool-2.4.6.tar.gz') )
+
+    self.assertEqual( ( 'gnu_libtool', '2.4.6', 0, 0, 'linux', 'release', [ 'x86_64' ], None ),
+                      f('linux/x86_64/release/gnu_libtool-2.4.6.tar.gz') )
+
+    self.assertEqual( ( 'gnu_libtool', '2.4.6', 0, 0, 'linux', 'debug', [ 'x86_64' ], None ),
+                      f('linux/x86_64/debug/gnu_libtool-2.4.6.tar.gz') )
     
 if __name__ == '__main__':
   unit_test.main()
