27d3ac40acd03bcfd1f84d89da33cfdc37157cd2
Ramiro Estrugo <ramiro@fateware.com>
2018-05-10 02:01:12 -0700

    add value_factory

---
+++ lib/rebuild/recipe/value/value_dir.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.common import check
+
+from .value_file import value_file
+
+class value_dir(value_file):
+
+  def __init__(self, env = None, origin = None, filename = '', properties = None):
+    'Class to manage a recipe dir.'
+    super(value_dir, self).__init__(env = env, origin = origin, filename = filename, properties = properties)
+
+check.register_class(value_dir, include_seq = True)
---
+++ lib/rebuild/recipe/value/value_factory.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from .value_registry import value_registry
+from .value_type import value_type
+
+class value_factory(object):
+
+  @classmethod
+  def create_with_class_name(clazz, env, origin, text, value_class_name):
+    check.check_recipe_load_env(env)
+    check.check_value_origin(origin)
+    check.check_string(text)
+    check.check_string(value_class_name)
+    value_class = value_registry.get(value_class_name)
+    if not value_class:
+      raise TypeError('%s: unknown value class \"%s\"' % (origin, value_class_name))
+    return value_class.parse(env, origin, text)
+  
+  @classmethod
+  def create_with_value_type(clazz, env, origin, text, vtype):
+    check.check_value_type(vtype)
+    value_class_name = value_type.value_to_name(vtype).lower()
+    return clazz.create_with_class_name(env, origin, text, value_class_name)
