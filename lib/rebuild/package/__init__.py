6f96509b66c1701b0e45bea7d13fba38d21a5479
Ramiro Estrugo <ramiro@fateware.com>
2018-05-15 22:18:11 -0700

    add

--- lib/rebuild/package/__init__.py
+++ lib/rebuild/package/__init__.py
@@ -4,12 +4,15 @@ from .artifact_db import artifact_db
 from .artifact_descriptor import artifact_descriptor
 from .artifact_descriptor_list import artifact_descriptor_list
 from .artifact_manager import artifact_manager
+from .env_dir import env_dir
+from .env_framework import env_framework
 from .package import package
 from .package_cli import package_cli
+from .package_files import package_files
 from .package_list import package_list
 from .package_manager import PackageFilesConflictError, PackageMissingRequirementsError
 from .package_manager import package_manager
 from .package_metadata import package_metadata
-from .package_files import package_files
 from .package_tester import package_tester
+
 
