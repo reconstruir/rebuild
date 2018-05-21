a75077fe40eb3458df7e702157795187fe0e1414
Ramiro Estrugo <ramiro@fateware.com>
2018-05-21 14:35:57 -0700

    convert step_result to be a namedtuple

--- lib/rebuild/step/step_result.py
+++ lib/rebuild/step/step_result.py
@@ -3,17 +3,20 @@
 import copy
 from bes.common import check
 
-class step_result(object):
+from collections import namedtuple
 
-  def __init__(self, success, message = None, failed_step = None, outputs = {}):
+class step_result(namedtuple('step_result', 'success, message, failed_step, outputs')):
+
+  def __new__(clazz, success, message = None, failed_step = None, outputs = None):
     check.check_bool(success)
-    self.success = success
     if message:
       check.check_string(message)
-    self.message = message
-    self.failed_step = failed_step
-    check.check_dict(outputs)
-    self.outputs = copy.deepcopy(outputs)
+    if outputs:
+      check.check_dict(outputs)
+      outputs = copy.deepcopy(outputs)
+    else:
+      outputs = {}
+    return clazz.__bases__[0].__new__(clazz, success, message, failed_step, outputs)
 
   def __str__(self):
     return '%s:%s:%s:%s' % (self.success, self.message, self.failed_step, self.outputs)
