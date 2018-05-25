e2875f1278aa8b090df555986c45326db9a63bdf
Ramiro Estrugo <ramiro@fateware.com>
2018-05-25 15:11:35 -0700

    make all members be properties

--- lib/rebuild/recipe/recipe_load_env.py
+++ lib/rebuild/recipe/recipe_load_env.py
@@ -7,12 +7,24 @@ class recipe_load_env(object):
   def __init__(self, build_target = None, downloads_manager = None, source_finder = None):
     if build_target:
       check.check_build_target(build_target)
-    self.build_target = build_target
+    self._build_target = build_target
     if downloads_manager:
       check.check_git_download_cache(downloads_manager)
-    self.downloads_manager = downloads_manager
+    self._downloads_manager = downloads_manager
     if source_finder:
       check.check_source_finder_chain(source_finder)
-    self.source_finder = source_finder
+    self._source_finder = source_finder
 
+  @property
+  def build_target(self):
+    return self._build_target
+
+  @property
+  def downloads_manager(self):
+    return self._downloads_manager
+
+  @property
+  def source_finder(self):
+    return self._source_finder
+  
 check.register_class(recipe_load_env, include_seq = False)
