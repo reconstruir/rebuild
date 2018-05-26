bc85f88c71656e01b0b2b61d355d71e85c13d32a
Ramiro Estrugo <ramiro@fateware.com>
2018-05-25 20:27:51 -0700

    add

---
+++ lib/rebuild/package/package_metadata_list.py
@@ -0,0 +1,35 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.common import check, type_checked_list
+from rebuild.base import package_descriptor_list
+from .package_metadata import package_metadata
+
+class package_metadata_list(type_checked_list):
+
+  __value_type__ = package_metadata
+  
+  def __init__(self, values = None):
+    super(package_metadata_list, self).__init__(values = values)
+
+#  def sort_by_descriptor(self):
+#    self._values = sorted(self._values, key = lambda package: package.descriptor)
+#    
+#  def descriptors(self):
+#    'Return the names for all the descriptors.'
+#    return package_descriptor_list([ p.descriptor for p in self ])
+#
+#  def latest_versions(self):
+#    'Return a list of only the lastest version of any package with multiple versions.'
+#    latest = {}
+#    for package in self:
+#      name = package.metadata.name
+#      if not name in latest:
+#        latest[name] = package
+#      else:
+#        if package.metadata.build_version > latest[name].metadata.build_version:
+#          latest[name] = package
+#    result = package_list(latest.values())
+#    result.sort_by_descriptor()
+#    return result
+  
+check.register_class(package_metadata_list, include_seq = False)
