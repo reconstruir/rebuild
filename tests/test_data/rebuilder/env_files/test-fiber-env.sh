61bb41fb786dc71fd23a30d632bbf05efc48cee4
Ramiro Estrugo <ramiro@fateware.com>
2018-06-24 15:04:16 -0700

    wip

--- tests/test_data/rebuilder/env_files/test-fiber-env.sh
+++ tests/test_data/rebuilder/env_files/test-fiber-env.sh
@@ -7,6 +7,8 @@ function test_fiber_env()
 {
   bes_assert "[ '$LIBFIBER_ENV1' = 'fiber_env1' ]"
   bes_assert "[ '$LIBFIBER_ENV2' = 'fiber_env2' ]"
+  bes_assert "[ '$LIBCARB_ENV1' = 'carb_env1' ]"
+  bes_assert "[ '$LIBCARB_ENV2' = 'carb_env2' ]"
 }
 
 bes_testing_run_unit_tests
