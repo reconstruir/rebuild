8676d79827406912d859f2a7919228ec0d6f94e0
Ramiro Estrugo <ramiro@fateware.com>
2018-06-24 20:05:54 -0700

    retire unused

--- tests/test_data/rebuilder/basic/shell_libs/libbar/libbar_env.sh
+++
@@ -1,6 +0,0 @@
-#@REBUILD_HEAD@
-
-export LIBBAR_ENV1=libbar_env1
-export LIBBAR_ENV2=libbar_env2
-
-#@REBUILD_TAIL@
--- tests/test_data/rebuilder/basic/shell_libs/libfoo/libfoo_env.sh
+++
@@ -1,6 +0,0 @@
-#@REBUILD_HEAD@
-
-export LIBFOO_ENV1=libfoo_env1
-export LIBFOO_ENV2=libfoo_env2
-
-#@REBUILD_TAIL@
