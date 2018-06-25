1f4e7fa5f3a9e5f49fdfd4ef3708af3276a0b7a8
Ramiro Estrugo <ramiro@fateware.com>
2018-06-24 19:20:37 -0700

    assert that env that doesnt belong is also null

--- tests/test_data/rebuilder/env_files/test-carb-env.sh
+++ tests/test_data/rebuilder/env_files/test-carb-env.sh
@@ -7,6 +7,12 @@ function test_carb_env()
 {
   bes_assert "[ '$LIBCARB_ENV1' = 'carb_env1' ]"
   bes_assert "[ '$LIBCARB_ENV2' = 'carb_env2' ]"
+  bes_assert "[ '$LIBFIBER_ENV1' = '' ]"
+  bes_assert "[ '$LIBFIBER_ENV2' = '' ]"
+  bes_assert "[ '$LIBFRUIT_ENV1' = '' ]"
+  bes_assert "[ '$LIBFRUIT_ENV2' = '' ]"
+  bes_assert "[ '$LIBWATER_ENV1' = '' ]"
+  bes_assert "[ '$LIBWATER_ENV2' = '' ]"
 }
 
 bes_testing_run_unit_tests
--- tests/test_data/rebuilder/env_files/test-fiber-env.sh
+++ tests/test_data/rebuilder/env_files/test-fiber-env.sh
@@ -5,10 +5,14 @@ source ${REBUILD_SHELL_FRAMEWORK_DIR}/env/bes_testing.sh
 
 function test_fiber_env()
 {
-  bes_assert "[ '$LIBFIBER_ENV1' = 'fiber_env1' ]"
-  bes_assert "[ '$LIBFIBER_ENV2' = 'fiber_env2' ]"
   bes_assert "[ '$LIBCARB_ENV1' = 'carb_env1' ]"
   bes_assert "[ '$LIBCARB_ENV2' = 'carb_env2' ]"
+  bes_assert "[ '$LIBFIBER_ENV1' = 'fiber_env1' ]"
+  bes_assert "[ '$LIBFIBER_ENV2' = 'fiber_env2' ]"
+  bes_assert "[ '$LIBFRUIT_ENV1' = '' ]"
+  bes_assert "[ '$LIBFRUIT_ENV2' = '' ]"
+  bes_assert "[ '$LIBWATER_ENV1' = '' ]"
+  bes_assert "[ '$LIBWATER_ENV2' = '' ]"
 }
 
 bes_testing_run_unit_tests
