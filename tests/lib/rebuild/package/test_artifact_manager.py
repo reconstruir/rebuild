a8a7d0ae3b0ee7706b6a6e89f6c677bfc7f4883b
Ramiro Estrugo <ramiro@fateware.com>
2018-10-23 21:41:14 -0700

    finish rewriting artifact manager unit tests such that no git shit is used.

--- tests/lib/rebuild/package/test_artifact_manager.py
+++ tests/lib/rebuild/package/test_artifact_manager.py
@@ -3,11 +3,9 @@
 
 import os.path as path
 from bes.testing.unit_test import unit_test
-from bes.fs import file_find, file_util, temp_file
-from bes.git import git
+from bes.fs import temp_file
 from rebuild.base import build_target, package_descriptor as PD
-from rebuild.package import artifact_manager, package, artifact_descriptor as AD
-from rebuild.package.unit_test_packages import unit_test_packages
+from rebuild.package import artifact_manager, artifact_descriptor as AD
 from rebuild.package.db_error import *
 from rebuild.package.fake_package_unit_test import fake_package_unit_test
 from rebuild.package import artifact_descriptor as AD
@@ -17,9 +15,8 @@ class test_artifact_manager(unit_test):
   DEBUG = unit_test.DEBUG
   #DEBUG = True
 
-  # These match what unit_test_packages use to make the testing packages
   LINUX_BT = build_target('linux', 'ubuntu', '18', 'x86_64', 'release')
-  MACOS_BT = build_target('macos', '', '10.10', 'x86_64', 'release')
+  MACOS_BT = build_target('macos', '', '10.14', 'x86_64', 'release')
 
   @classmethod
   def _make_empty_artifact_manager(clazz):
@@ -29,19 +26,8 @@ class test_artifact_manager(unit_test):
     am = artifact_manager(root_dir)
     return am
   
-  @classmethod
-  def _make_test_artifact_manager(clazz, address = None, items = None):
-    root_dir = temp_file.make_temp_dir(delete = not clazz.DEBUG)
-    if clazz.DEBUG:
-      print("root_dir:\n%s\n" % (root_dir))
-    am = artifact_manager(root_dir, address = address)
-    if items:
-      clazz._make_test_artifacts(items, am.root_dir)
-    unit_test_packages.publish_artifacts(am)
-    return am
-
   def test_artifact_path(self):
-    am = self._make_test_artifact_manager()
+    am = self._make_empty_artifact_manager()
     pi = PD('foo', '1.2.34-1')
     self.assertEqual( path.join(am.root_dir, pi.artifact_path(self.LINUX_BT)), am.artifact_path(pi, self.LINUX_BT) )
 
@@ -86,24 +72,9 @@ fake_package apple 1.2.3 1 0 linux release x86_64 ubuntu 18
     with self.assertRaises(AlreadyInstalledError) as context:
       am.publish(tmp_tarball, self.LINUX_BT, False)
     
-  def _make_test_artifacts_git_repo(self):
-    tmp_repo = temp_file.make_temp_dir(delete = not self.DEBUG)
-    if self.DEBUG:
-      print("tmp_repo:\n%s\n" % (tmp_repo))
-    self._make_test_artifacts(unit_test_packages.TEST_PACKAGES, tmp_repo)
-    #unit_test_packages.publish_artifacts(am)
-    assert not git.is_repo(tmp_repo)
-    git.init(tmp_repo)
-    git.add(tmp_repo, '.')
-    git.commit(tmp_repo, 'unittest', '.')
-    return self._make_test_artifact_manager(address = tmp_repo)
-
-  @classmethod
-  def _make_test_artifacts(clazz, items, root_dir):
-    unit_test_packages.make_test_packages(items, root_dir)
-    
   def test_find_by_package_descriptor_linux(self):
-    am = self._make_test_artifacts_git_repo()
+    mutations = { 'system': 'linux', 'distro': 'ubuntu', 'distro_version': '18' }
+    am = self._create_test_artifact_manager_with_packages(self.LINUX_BT, mutations)
     self.assertEqual( 'water-1.0.0', am.find_by_package_descriptor(PD('water', '1.0.0'), self.LINUX_BT).package_descriptor.full_name )
     self.assertEqual( 'water-1.0.0-1', am.find_by_package_descriptor(PD('water', '1.0.0-1'), self.LINUX_BT).package_descriptor.full_name )
     self.assertEqual( 'water-1.0.0-2', am.find_by_package_descriptor(PD('water', '1.0.0-2'), self.LINUX_BT).package_descriptor.full_name )
@@ -114,7 +85,8 @@ fake_package apple 1.2.3 1 0 linux release x86_64 ubuntu 18
     self.assertEqual( 'pear_juice-6.6.6', am.find_by_package_descriptor(PD('pear_juice', '6.6.6'), self.LINUX_BT).package_descriptor.full_name )
 
   def test_find_by_package_descriptor_macos(self):
-    am = self._make_test_artifacts_git_repo()
+    mutations = { 'system': 'macos', 'distro': '', 'distro_version': '10.14' }
+    am = self._create_test_artifact_manager_with_packages(self.MACOS_BT, mutations)
     self.assertEqual( 'water-1.0.0', am.find_by_package_descriptor(PD('water', '1.0.0'), self.MACOS_BT).package_descriptor.full_name )
     self.assertEqual( 'water-1.0.0-1', am.find_by_package_descriptor(PD('water', '1.0.0-1'), self.MACOS_BT).package_descriptor.full_name )
     self.assertEqual( 'water-1.0.0-2', am.find_by_package_descriptor(PD('water', '1.0.0-2'), self.MACOS_BT).package_descriptor.full_name )
@@ -147,22 +119,22 @@ fake_package apple 1.2.3 1 0 linux release x86_64 ubuntu 18
 
   def test_list_latest_versions_macos(self):
     self.maxDiff = None
-    mutations = { 'system': 'macos', 'distro': 'none', 'distro_version': '10.14' }
+    mutations = { 'system': 'macos', 'distro': '', 'distro_version': '10.14' }
     am = self._create_test_artifact_manager_with_packages(self.MACOS_BT, mutations)
     expected = [
-      AD('apple', '1.2.3', 1, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('arsenic', '1.2.9', 1, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('citrus', '1.0.0', 2, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('fiber', '1.0.0', 0, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('fructose', '3.4.5', 6, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('fruit', '1.0.0', 0, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('mercury', '1.2.9', 0, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('orange', '6.5.4', 3, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('orange_juice', '1.4.5', 0, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('pear', '1.2.3', 1, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('pear_juice', '6.6.6', 0, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('smoothie', '1.0.0', 0, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
-      AD('water', '1.0.0', 2, 0, 'macos', 'release', 'x86_64', 'none', '10.14'),
+      AD('apple', '1.2.3', 1, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('arsenic', '1.2.9', 1, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('citrus', '1.0.0', 2, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('fiber', '1.0.0', 0, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('fructose', '3.4.5', 6, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('fruit', '1.0.0', 0, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('mercury', '1.2.9', 0, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('orange', '6.5.4', 3, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('orange_juice', '1.4.5', 0, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('pear', '1.2.3', 1, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('pear_juice', '6.6.6', 0, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('smoothie', '1.0.0', 0, 0, 'macos', 'release', 'x86_64', '', '10.14'),
+      AD('water', '1.0.0', 2, 0, 'macos', 'release', 'x86_64', '', '10.14'),
     ]
     self.assertEqual( expected, am.list_latest_versions(self.MACOS_BT) )
 
