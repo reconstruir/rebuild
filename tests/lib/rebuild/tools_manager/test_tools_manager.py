ea5985d062f680d2580614716c813715908f5b4c
Ramiro Estrugo <ramiro@fateware.com>
2018-10-24 20:15:41 -0700

    fix for new unit tests

--- tests/lib/rebuild/tools_manager/test_tools_manager.py
+++ tests/lib/rebuild/tools_manager/test_tools_manager.py
@@ -9,18 +9,21 @@ from rebuild.base import build_target, build_system, build_level, package_descri
 from rebuild.package import artifact_manager
 from rebuild.tools_manager import tools_manager
 from rebuild.package.unit_test_packages import unit_test_packages
+from rebuild.package.fake_package_unit_test import fake_package_unit_test as FPUT
 
 class test_tools_manager(unit_test):
 
   DEBUG = unit_test.DEBUG
   DEBUG = True
 
+  TEST_BUILD_TARGET = build_target.parse_path('linux-ubuntu-18/x86_64/release')
+
   def _make_test_tm(self):
     root_dir = temp_file.make_temp_dir(delete = not self.DEBUG)
     tools_dir = path.join(root_dir, 'tools')
     if self.DEBUG:
       print("\ntools_dir:\n", tools_dir)
-    return tools_manager(tools_dir)
+    return tools_manager(tools_dir, self.TEST_BUILD_TARGET)
 
   @classmethod
   def _make_test_artifact_manager(clazz):
@@ -43,18 +46,30 @@ class test_tools_manager(unit_test):
     tm.update(packages, am)
 
   def test_install_and_use_a_tool(self):
+    recipe = '''
+fake_package mytool 1.2.3 1 0 linux release x86_64 ubuntu 18
+  files
+    bin/mytool.sh
+      \#!/bin/bash
+      echo mytool.sh
+      exit 0
+'''
     tm = self._make_test_tm()
-    am = self._make_test_artifact_manager()
-    water_desc = package_descriptor.parse('water-1.0.0-0')
+    bt = build_target.parse_path('linux-ubuntu-18/x86_64/release')
+    am = FPUT.make_artifact_manager()
+    mutations = { 'system': 'linux', 'distro': 'ubuntu', 'distro_version': '18' }
+    tmp_tarball = FPUT.create_one_package(recipe, mutations)
+    am.publish(tmp_tarball, bt, False)
+    desc = package_descriptor.parse('mytool-1.2.3-1')
     packages = [
-      water_desc
+      desc
     ]
     tm.update(packages, am)
-    tool_name = water_desc.name + '_script.sh'
-    exe = tm.tool_exe(water_desc, tool_name)
+    tool_name = 'mytool.sh'
+    exe = tm.tool_exe(desc, 'mytool.sh')
     rv = execute.execute(exe)
     self.assertEqual( 0, rv.exit_code )
-    self.assertEqual( water_desc.full_name, rv.stdout.strip() )
+    self.assertEqual( 'mytool.sh', rv.stdout.strip() )
 
 if __name__ == '__main__':
   unit_test.main()
