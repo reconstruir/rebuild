c38a23ac22200cd2f2d2727ba98d5d8c622d0503
Ramiro Estrugo <ramiro@fateware.com>
2018-10-27 11:48:16 -0700

    add

---
+++ lib/rebuild/package/fake_package_source.py
@@ -0,0 +1,32 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from os import path
+from collections import namedtuple
+from bes.common import check, node
+from bes.text import white_space
+#from bes.fs import file_util, temp_file
+
+class fake_package_source(namedtuple('fake_package_source', 'filename, source_code')):
+  'Class to describe fake source code for a package.'
+  
+  def __new__(clazz, filename, source_code):
+    check.check_string(filename)
+    check.check_string(source_code)
+    return clazz.__bases__[0].__new__(clazz, filename, source_code)
+
+  def __str__(self):
+    return self.to_string()
+
+  def to_string(self, depth = 0, indent = 2):
+    s = self._to_node().to_string(depth = depth, indent = indent).strip()
+    return white_space.shorten_multi_line_spaces(s)
+  
+  def _to_node(self):
+    'A convenient way to make a string is to build a graph first.'
+    root = node(self.filename)
+    for line in self.source_code.split('\n'):
+      root.add_child(line)
+    return root
+
+  def compile(self, object):
+    assert False
---
+++ tests/lib/rebuild/package/test_fake_package_source.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from bes.testing.unit_test import unit_test
+from rebuild.package.fake_package_source import fake_package_source as S
+
+from bes.fs import temp_file, temp_item
+
+class test_fake_package_source(unit_test):
+
+  DEBUG = unit_test.DEBUG
+  #DEBUG = True
+
+  def test___str__(self):
+    s = S('foo.c', '''\#include <stdio.h>\nint main(int argc, char* argv[]) {\n  printf("foo.c\\n");\n  return 0;\n}\n''')
+    expected = '''foo.c
+  \#include <stdio.h>
+  int main(int argc, char* argv[]) {
+    printf("foo.c\\n");
+    return 0;
+  }'''
+    
+    self.assertMultiLineEqual( expected, str(s) )
+
+if __name__ == '__main__':
+  unit_test.main()
