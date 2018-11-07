a582d3b848bbb9a78dbb6f1a4af44fac25e8734a
Ramiro Estrugo <ramiro@fateware.com>
2018-11-06 16:01:12 -0800

    now environment is checked and used to execute an exe

--- tests/lib/rebuild/tools_manager/test_new_tools_manager.py
+++ tests/lib/rebuild/tools_manager/test_new_tools_manager.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-import os.path as path
+import copy, os.path as path
 from bes.testing.unit_test import unit_test
-from bes.common import cached_property
-from bes.fs import temp_file
-from bes.system import execute
+from bes.common import cached_property, dict_util, string_util
+from bes.fs import file_find, temp_file
+from bes.system import execute, os_env
 from rebuild.base import build_target, build_system, build_level, package_descriptor as PD
 from rebuild.tools_manager import new_tools_manager as TM
 from _rebuild_testing.fake_package_unit_test import fake_package_unit_test as FPUT
@@ -20,7 +20,7 @@ class test_new_tools_manager(unit_test):
   __script__ = __file__, '../../../../bin/rebuilder.py'
   
   DEBUG = unit_test.DEBUG
-  DEBUG = True
+  #DEBUG = True
 
   TEST_BUILD_TARGET = build_target.parse_path('linux-ubuntu-18/x86_64/release')
 
@@ -99,14 +99,23 @@ fake_package carbon 1.0.0 0 0 linux release x86_64 ubuntu 18
 
 fake_package steel 1.0.0 0 0 linux release x86_64 ubuntu 18
   files
-    bin/steel.py
+    bin/steel_exe.py
       \#!/usr/bin/env python
-      print('fbar')
+      import sys
+      assert len(sys.argv) == 2
+      import steel
+      print('steel_exe.py: %d' % (steel.steel_func1(int(sys.argv[1]))))
       raise SystemExit(0)
+    lib/python/steel.py
+      def steel_func1(x):
+        return x + 10
 
   env_files
     steel_env.sh
+      \#@REBUILD_HEAD@
       export STEEL_ENV1=steel_env1
+      export PYTHONPATH=${PYTHONPATH}:${REBUILD_STUFF_DIR}/lib/python
+      \#@REBUILD_TAIL@
 
   requirements
     all: RUN iron >= 1.0.0
@@ -137,14 +146,67 @@ fake_package cuchillo 1.0.0 0 0 linux release x86_64 ubuntu 18
     amt.publish('wood;1.0.0;0;0;linux;release;x86_64;ubuntu;18')
     cuchillo = PD.parse('cuchillo-1.0.0')
     tm.ensure_tool(cuchillo)
-    env = tm.transform_env(cuchillo, {})
+    env = tm.transform_env(cuchillo, os_env.clone_current_env())
+    replacements = { tm.root_dir: '$ROOT_DIR' }
+    env2 = copy.deepcopy(env)
+    dict_util.replace_values(env2, replacements)
     self.assertEqual( {
       'WOOD_ENV1': 'wood_env1',
       'CUCHILLO_ENV1': 'cuchillo_env1',
       'STEEL_ENV1': 'steel_env1',
       'IRON_ENV1': 'iron_env1',
       'CARBON_ENV1': 'carbon_env1',
-    }, env )
+      'PYTHONPATH': ':$ROOT_DIR/steel_1_0_0/linux-ubuntu-18/x86_64/stuff/lib/python',
+      '_BES_DEV_ROOT': '$ROOT_DIR/steel_1_0_0/linux-ubuntu-18/x86_64/env/framework',
+    }, env2 )
+
+    self.assertEqual( [
+      'cuchillo_1_0_0/linux-ubuntu-18/x86_64/db/packages.db',
+      'cuchillo_1_0_0/linux-ubuntu-18/x86_64/env/cuchillo_env.sh',
+      'cuchillo_1_0_0/linux-ubuntu-18/x86_64/env/framework/bin/bes_path.py',
+      'cuchillo_1_0_0/linux-ubuntu-18/x86_64/env/framework/env/bes_framework.sh',
+      'cuchillo_1_0_0/linux-ubuntu-18/x86_64/env/framework/env/bes_path.sh',
+      'cuchillo_1_0_0/linux-ubuntu-18/x86_64/env/framework/env/bes_testing.sh',
+      'cuchillo_1_0_0/linux-ubuntu-18/x86_64/run.sh',
+      'cuchillo_1_0_0/linux-ubuntu-18/x86_64/setup.sh',
+      'cuchillo_1_0_0/linux-ubuntu-18/x86_64/stuff/bin/cuchillo.py',
+      'cuchillo_1_0_0/run.sh',
+      'cuchillo_1_0_0/setup.sh',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/db/packages.db',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/env/carbon_env.sh',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/env/framework/bin/bes_path.py',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/env/framework/env/bes_framework.sh',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/env/framework/env/bes_path.sh',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/env/framework/env/bes_testing.sh',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/env/iron_env.sh',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/env/steel_env.sh',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/run.sh',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/setup.sh',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/stuff/bin/carbon.py',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/stuff/bin/iron.py',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/stuff/bin/steel_exe.py',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/stuff/lib/python/easy-install.pth',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/stuff/lib/python/site.py',
+      'steel_1_0_0/linux-ubuntu-18/x86_64/stuff/lib/python/steel.py',
+      'steel_1_0_0/run.sh',
+      'steel_1_0_0/setup.sh',
+      'wood_1_0_0/linux-ubuntu-18/x86_64/db/packages.db',
+      'wood_1_0_0/linux-ubuntu-18/x86_64/env/framework/bin/bes_path.py',
+      'wood_1_0_0/linux-ubuntu-18/x86_64/env/framework/env/bes_framework.sh',
+      'wood_1_0_0/linux-ubuntu-18/x86_64/env/framework/env/bes_path.sh',
+      'wood_1_0_0/linux-ubuntu-18/x86_64/env/framework/env/bes_testing.sh',
+      'wood_1_0_0/linux-ubuntu-18/x86_64/env/wood_env.sh',
+      'wood_1_0_0/linux-ubuntu-18/x86_64/run.sh',
+      'wood_1_0_0/linux-ubuntu-18/x86_64/setup.sh',
+      'wood_1_0_0/linux-ubuntu-18/x86_64/stuff/bin/wood.py',
+      'wood_1_0_0/run.sh',
+      'wood_1_0_0/setup.sh',
+      ], file_find.find(tm.root_dir) )
+    
+    exe = tm.tool_exe(PD.parse('steel-1.0.0'), 'steel_exe.py')
+    rv = execute.execute([ exe, '6' ], env = env)
+    self.assertEqual( 0, rv.exit_code )
+    self.assertEqual( 'steel_exe.py: 16', rv.stdout.strip() )
     
   def xtest_use_binary_tool_with_shared_lib(self):
     tm, am, amt = self._make_test_tm()
