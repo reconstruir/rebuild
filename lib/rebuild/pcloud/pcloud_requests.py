ca8d72bfed24becdf5f1a44da12e1c90cb58e71b
Ramiro Estrugo <ramiro@fateware.com>
2018-12-05 20:34:48 -0800

    cleanup requests by using url_util.

--- lib/rebuild/pcloud/pcloud_requests.py
+++ lib/rebuild/pcloud/pcloud_requests.py
@@ -1,20 +1,10 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-import json, urllib
-
-from bes.system import compat
-
-if compat.IS_PYTHON2:
-  from urllib2 import urlopen, Request
-  from urllib import urlencode
-else:
-  from urllib.request import urlopen, Request
-  from urllib.parse import urlencode
-  
+import json
 from collections import namedtuple
 
 from bes.compat.url_compat import urljoin
-
+from bes.common import url_util
 
 class pcloud_requests(object):
 
@@ -25,43 +15,19 @@ class pcloud_requests(object):
   @classmethod
   def get(clazz, api_method, params):
     url = clazz.make_api_url(api_method)
-    data = urlencode(params).encode('utf-8')
-    req = Request(url, data = data)
-    response = urlopen(req)
-    content = response.read()
-    status_code = response.getcode()
-    if status_code == 200:
-      payload = json.loads(content)
+    response = url_util.get(url, params = params)
+    if response.status_code == 200:
+      payload = json.loads(response.content)
     else:
       payload = None
-    return clazz._response(url, status_code, payload)
+    return clazz._response(url, response.status_code, payload)
 
   @classmethod
-  def download_to_stream(clazz, links, stream, file_path = None, file_id = None):
-    assert file_path or file_id
+  def download_to_stream(clazz, links, stream):
     url = 'https://{host}{path}'.format(host = links.hosts[0], path = links.path)
-    response = urlopen(url)
-    status_code = response.getcode()
-    if status_code != 200:
-      return clazz._response(url, status_code, None)
-    chunk_size = 1024
-    while True:
-      chunk = response.read(chunk_size)
-      if not chunk:
-        break
-      stream.write(chunk)
+    status_code = url_util.download_to_stream(url, stream)
     return clazz._response(url, status_code, None)
 
   @classmethod
   def make_api_url(clazz, method):
     return urljoin(clazz.API, method)
-
-  '''
-  @classmethod
-  def upload(clazz, params, files):
-    url = pcloud_requests.make_api_url('uploadfile')
-    from .foo import MultiPartForm
-    form = MultiPartForm()
-    for key, value im params:
-      form.add_field(key, value)
-'''
