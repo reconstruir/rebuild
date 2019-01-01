db4ec6ed32cce8f1f6692ba8cc948f19fc7a2510
Ramiro Estrugo <ramiro@fateware.com>
2019-01-01 05:38:48 -0800

    work in progress to get the project file manager working.

--- lib/rebuild/project/project_file_manager.py
+++ lib/rebuild/project/project_file_manager.py
@@ -1,15 +1,86 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
+from os import path
+from collections import namedtuple
 from bes.common import check
 
 from bes.system import os_env_var
-from bes.fs import file_find, file_path
+from bes.fs import file_find, file_path, file_util
+from bes.dependency import dependency_resolver
+
+from .project_file import project_file
+from .project_file_parser import project_file_parser
+
+class _project_entry(namedtuple('_project_entry', 'name, filename, project_file')):
+
+  def __new__(clazz, name, filename, project_file):
+    check.check_string(name)
+    check.check_string(filename)
+    check.check_project_file(project_file)
+    return clazz.__bases__[0].__new__(clazz, name, filename, project_file)
 
 class project_file_manager(object):
 
   def __init__(self):
-    pass
+    self._filename_map = {}
+    self._projects = {}
 
+  def load_project_file(self, filename):
+    if filename in self._filename_map:
+      raise RuntimeError('Already loaded: %s' % (filename))
+    if not project_file.is_project_file(filename):
+      raise RuntimeError('Not a project file: %s' % (filename))
+    text = file_util.read(filename)
+    parser = project_file_parser(filename, text)
+    project_files = parser.parse()
+    for pf in project_files:
+      name = pf.name
+      if name in self._projects:
+        raise RuntimeError('Duplicate project \"%s\" found in: %s' % (name, filename))
+      self._projects[name] = _project_entry(name, filename, pf)
+    self._filename_map[filename] = project_files
+
+  def print_projects(self):
+    for name in sorted(self._projects.keys()):
+      entry = self._projects[name]
+      print('%s - %s\n%s\n' % (entry.name, entry.filename, str(entry.project_file)))
+      
+  def print_dep_map(self, build_target):
+    check.check_build_target(build_target)
+    dep_map = self._make_dep_map(self._projects, build_target)
+    for name, imports in sorted(dep_map.items()):
+      print('%s: %s' % (name, ' '.join(list(imports))))
+
+  def resolve(self, filename, build_target):
+    check.check_build_target(build_target)
+    project_files = self._filename_map.get(filename, None)
+    if not project_files:
+      raise RuntimeError('Filename not loaded: %s' % (filename))
+    names = [ pf.name for pf in project_files ]
+    dep_map = self._make_dep_map(self._projects, build_target)
+    resolved_names = dependency_resolver.resolve_deps(dep_map, names)
+    return [ self._projects[name].project_file for name in resolved_names ]
+
+  def available_recipes(self, filename, build_target):
+    check.check_build_target(build_target)
+    recipes = []
+    project_files = self.resolve(filename, build_target)
+    for pf in project_files:
+      more_recipes = pf.resolve_recipes(build_target.system)
+      more_recipes = [ path.join(path.dirname(pf.filename), r) for r in more_recipes ]
+      recipes.extend(more_recipes)
+    return recipes
+  
+  @classmethod
+  def _make_dep_map(clazz, projects, build_target):
+    dep_map = {}
+    for name in projects.iterkeys():
+      entry = projects[name]
+      imports = entry.project_file.resolve_imports(build_target.system)
+      dep_map[name] = set(imports)
+    return dep_map
+
+  
   @classmethod
   def find_env_project_files(clazz):
     return file_path.glob(os_env_var('REBUILD_RECIPE_PATH').path, '*.reproject')
--- tests/lib/rebuild/project/test_project_file_manager.py
+++ tests/lib/rebuild/project/test_project_file_manager.py
@@ -5,6 +5,7 @@ import os.path as path
 from bes.testing.unit_test import unit_test
 from rebuild.project.project_file import project_file
 from rebuild.project.project_file_manager import project_file_manager as PFM
+from rebuild.base import build_target as BT
 from bes.system import os_env_var
 from bes.fs import file_util, temp_file
 from bes.fs.testing import temp_content
@@ -19,44 +20,86 @@ class test_project_file_manager(unit_test):
     tmp_dir = self._make_tmp_project_files()
 
     os_env_var('REBUILD_RECIPE_PATH').path = [
+      path.join(tmp_dir, 'fructose'),
       path.join(tmp_dir, 'kiwi'),
       path.join(tmp_dir, 'orange'),
     ]
 
     try:
       self.assertEqual( [
+        path.join(tmp_dir, 'fructose/fructose.reproject'),
         path.join(tmp_dir, 'kiwi/kiwi.reproject'),
         path.join(tmp_dir, 'orange/orange.reproject'),
       ], PFM.find_env_project_files() )
     finally:
       os_env_var('REBUILD_RECIPE_PATH').unset
 
+  def test_load_project_file(self):
+    tmp_dir = self._make_tmp_project_files()
+    bt = BT.parse_path('macos-10.10/x86_64/release')
+    p = PFM()
+    p.load_project_file(path.join(tmp_dir, 'fructose/fructose.reproject'))
+    p.load_project_file(path.join(tmp_dir, 'kiwi/kiwi.reproject'))
+    p.load_project_file(path.join(tmp_dir, 'orange/orange.reproject'))
+
+    smoothie_project = '''!rebuild.project!
+project smoothie
+  recipes
+    recipes/smoothie/mysmoothie.recipe
+  imports
+    kiwi
+'''
+    smoothie_pf = temp_file.make_temp_file(content = smoothie_project)
+    p.load_project_file(smoothie_pf)
+    available = p.available_recipes(smoothie_pf, bt)
+    for r in available:
+      print('RECIPE: %s' % (str(r)))
+      
+    #p.print_projects()
+    #p.print_dep_map()
+    
   def _make_tmp_project_files(self):
     tmp_dir = temp_file.make_temp_dir(delete = not self.DEBUG)
     if self.DEBUG:
       print('tmp_dir: %s' % (tmp_dir))
 
+    fructose_project = '''!rebuild.project!
+project fructose
+  recipes
+    recipes/fructose/myfructose.recipe
+'''
+      
     kiwi_project = '''!rebuild.project!
 project kiwi
   recipes
     recipes/kiwi/mykiwi.recipe
+  imports
+    fructose
+
+project citrus
+  recipes
+    recipes/citrus/mycitrus.recipe
 '''
-    tmp_kiwi_file = temp_file.make_temp_file(content = kiwi_project)
     
     orange_project = '''!rebuild.project!
 project orange
   recipes
     recipes/orange/myorange.recipe
+  imports
+    fructose
 
 project lemon
   recipes
     recipes/lemon/mylemon.recipe
+  imports
+    fructose
+    citrus
 '''
-    tmp_orange_file = temp_file.make_temp_file(content = orange_project)
     
     items = [
-      'file kiwi/kiwi.reproject "{project_file}" 644'.format(project_file = tmp_kiwi_file),
-      'file orange/orange.reproject "{project_file}" 644'.format(project_file = tmp_orange_file),
+      'file fructose/fructose.reproject "{project_file}" 644'.format(project_file = temp_file.make_temp_file(content = fructose_project)),
+      'file kiwi/kiwi.reproject "{project_file}" 644'.format(project_file = temp_file.make_temp_file(content = kiwi_project)),
+      'file orange/orange.reproject "{project_file}" 644'.format(project_file = temp_file.make_temp_file(content = orange_project)),
     ]
     temp_content.write_items(items, tmp_dir)
     return tmp_dir
