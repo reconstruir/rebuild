4d8cb3adc13e77a5a200e57156b975eb96982e01
Ramiro Estrugo <ramiro@fateware.com>
2019-01-05 20:34:40 -0800

    move _test_context to its own venv_tester class and update test_venv_manager.

---
+++ lib/_rebuild_testing/venv_tester.py
@@ -0,0 +1,69 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+import os.path as path
+
+from bes.fs import file_util, temp_file
+
+from rebuild.base import build_target
+from rebuild.venv.venv_config import venv_config
+from rebuild.venv.venv_manager import venv_manager
+
+from _rebuild_testing.artifact_manager_tester import artifact_manager_tester
+
+class venv_tester(object):
+
+  _STORAGE_CONFIG_TEMPLATE = '''\
+!rebuild.revenv!
+config
+  storage
+    name: unit_test_storage
+    provider: local
+    location: {artifacts_dir}
+'''
+
+  def __init__(self, config, recipes = None, debug = False):
+    self.tmp_dir = self._make_temp_dir(debug)
+    self.amt = artifact_manager_tester(recipes = recipes)
+    if recipes:
+      self.amt.publish(recipes)
+    self.config_filename = path.join(self.tmp_dir, 'config.revenv')
+    self._write_config_file(self.config_filename, config, self.amt.am)
+    self.build_target = build_target.parse_path('linux-ubuntu-18/x86_64/release')
+    config_obj = venv_config.load(self.config_filename, self.build_target)
+    self.manager = venv_manager(config_obj, self.amt.am, self.build_target, self.tmp_dir)
+  
+  @classmethod
+  def _make_temp_dir(clazz, debug):
+    tmp_dir = temp_file.make_temp_dir(delete = not debug)
+    if debug:
+      print("tmp_dir: ", tmp_dir)
+    return tmp_dir
+
+  @classmethod
+  def _write_config_file(clazz, filename, config, artifact_manager):
+    storage_content = clazz._STORAGE_CONFIG_TEMPLATE.format(artifacts_dir = artifact_manager._root_dir)
+    content = config.format(head = storage_content)
+    file_util.save(filename, content = content)
+  
+  def installed_packages(self, project_name, include_version = False):
+    return self.manager.installed_packages_names(project_name, self.build_target, include_version = include_version)
+
+  def update_from_config(self, project_name, options = None):
+    return self.manager.update_from_config(project_name, self.build_target, options = options)
+
+  def clear_project_from_config(self, project_name):
+    return self.manager.clear_project_from_config(project_name, self.build_target)
+
+  def rewrite_config(self, config):
+    self._write_config_file(self.config_filename, config, self.amt.am)
+    config_obj = venv_config.load(self.config_filename, self.build_target)
+    self.manager._config = config_obj
+
+  def publish_artifacts(self, adescs, mutations = {}):
+    self.amt.publish(adescs, mutations = mutations)
+    
+  def add_recipes(self, recipes):
+    self.amt.add_recipes(recipes)
+    
+  def clear_artifacts(self):
+    self.amt.retire_all()
--- tests/lib/rebuild/venv/test_venv_manager.py
+++ tests/lib/rebuild/venv/test_venv_manager.py
@@ -8,29 +8,22 @@ from collections import namedtuple
 from bes.testing.unit_test import unit_test
 from bes.system import os_env, execute
 from bes.fs import file_util, temp_file
-from _rebuild_testing.fake_package_unit_test import fake_package_unit_test as FPUT
-from _rebuild_testing.fake_package_recipes import fake_package_recipes as RECIPES
-from rebuild.base import build_target as BT
 
+from rebuild.base import build_target as BT
 from rebuild.venv.venv_config import venv_config
 from rebuild.venv.venv_manager import venv_manager
 from rebuild.venv.venv_install_options import venv_install_options
-  
-class test_manager(unit_test):
+
+from _rebuild_testing.fake_package_recipes import fake_package_recipes as RECIPES
+from _rebuild_testing.artifact_manager_tester import artifact_manager_tester as AMT
+from _rebuild_testing.venv_tester import venv_tester
+
+class test_venv_manager(unit_test):
 
   __unit_test_data_dir__ = '${BES_TEST_DATA_DIR}/remanager'
 
   DEBUG = False
 #  DEBUG = True
-
-  _HEAD_TEMPLATE = '''\
-!rebuild.revenv!
-config
-  storage
-    name: unit_test_storage
-    provider: local
-    location: {artifacts_dir}
-'''
 
   def test_packages_print_empty(self):
     config = '''{head}
@@ -42,7 +35,7 @@ projects
     packages
       fiber
 '''
-    test = self._setup_test(config, recipes = RECIPES.FOODS)
+    test = venv_tester(config, recipes = RECIPES.FOODS)
     self.assertEqual( [], test.installed_packages('test1') )
 
   def test_packages_update(self):
@@ -55,7 +48,7 @@ projects
     packages
       fiber
 '''
-    test = self._setup_test(config, recipes = RECIPES.FOODS)
+    test = venv_tester(config, recipes = RECIPES.FOODS)
     test.update_from_config('test1')
     self.assertEqual( [ 'water' ], test.installed_packages('test1') )
 
@@ -78,7 +71,7 @@ projects
     packages
       water fiber orange_juice
 '''
-    test = self._setup_test(config1, recipes = RECIPES.FOODS)
+    test = venv_tester(config1, recipes = RECIPES.FOODS)
     test.update_from_config('test')
     self.assertEqual( [ 'water' ], test.installed_packages('test') )
 
@@ -103,7 +96,7 @@ projects
     packages
       pear_juice
 '''
-    test = self._setup_test(config1, recipes = RECIPES.FOODS)
+    test = venv_tester(config1, recipes = RECIPES.FOODS)
     test.update_from_config('test')
     self.assertEqual( [ 'citrus', 'fiber', 'fructose', 'fruit', 'orange', 'orange_juice', 'water' ], test.installed_packages('test') )
 
@@ -135,7 +128,7 @@ projects
   test
     packages
 '''
-    test = self._setup_test(config1, recipes = RECIPES.FOODS)
+    test = venv_tester(config1, recipes = RECIPES.FOODS)
     test.update_from_config('test')
     self.assertEqual( [ 'arsenic', 'mercury', 'water' ], test.installed_packages('test') )
 
@@ -158,13 +151,13 @@ projects
     packages
       arsenic mercury water
 '''
-    test = self._setup_test(config1, recipes = RECIPES.FOODS)
+    test = venv_tester(config1, recipes = RECIPES.FOODS)
     test.update_from_config('test')
     self.assertEqual( [ 'arsenic', 'mercury', 'water' ], test.installed_packages('test') )
     test.clear_project_from_config('test')
     self.assertEqual( [], test.installed_packages('test') )
     
-  def xtest_packages_update_upgrade(self):
+  def test_packages_update_upgrade(self):
     recipes1 = '''
 fake_package aflatoxin 1.0.9 0 0 linux release x86_64 ubuntu 18
 '''
@@ -177,17 +170,20 @@ projects
     packages
       aflatoxin
 '''
-    test = self._setup_test(config, recipes = None)
-    test.publish_artifacts(recipes1)
+    test = venv_tester(config)
 
+    test.add_recipes(recipes1)
+    test.publish_artifacts(recipes1)
+    
     test.update_from_config('test')
     self.assertEqual( [ 'aflatoxin-1.0.9' ], test.installed_packages('test', include_version = True) )
 
+    test.add_recipes(recipes2)
     test.publish_artifacts(recipes2)
 
     test.update_from_config('test')
     self.assertEqual( [ 'aflatoxin-1.0.10' ], test.installed_packages('test', include_version = True) )
-
+    
   def test_packages_update_downgrade(self):
     recipes1 = '''
 fake_package aflatoxin 1.0.10 0 0 linux release x86_64 ubuntu 18
@@ -201,11 +197,16 @@ projects
     packages
       aflatoxin
 '''
-    test = self._setup_test(config, recipes = recipes1)
+    test = venv_tester(config)
+    test.add_recipes(recipes1)
+    test.publish_artifacts(recipes1)
+    
     test.update_from_config('test')
     self.assertEqual( [ 'aflatoxin-1.0.10' ], test.installed_packages('test', include_version = True) )
 
     test.clear_artifacts()
+    
+    test.add_recipes(recipes2)
     test.publish_artifacts(recipes2)
 
     test.update_from_config('test', options = venv_install_options(allow_downgrade = False))
@@ -213,7 +214,6 @@ projects
 
     test.update_from_config('test', options = venv_install_options(allow_downgrade = True))
     self.assertEqual( [ 'aflatoxin-1.0.9' ], test.installed_packages('test', include_version = True) )
-
     
   def test_packages_update_specific_version(self):
     recipes1 = '''
@@ -227,7 +227,7 @@ projects
     packages
       aflatoxin == 1.0.1
 '''
-    test = self._setup_test(config, recipes = recipes1)
+    test = venv_tester(config, recipes = recipes1)
     test.update_from_config('test')
     self.assertEqual( [ 'aflatoxin-1.0.1' ], test.installed_packages('test', include_version = True) )
     
@@ -238,25 +238,6 @@ projects
       print("tmp_dir: ", tmp_dir)
     return tmp_dir
 
-  @classmethod
-  def _make_test_artifact_manager(clazz, recipes = None):
-    mutations = { 'system': 'linux', 'distro': 'ubuntu', 'distro_version': '18' }
-    return FPUT.make_artifact_manager(debug = clazz.DEBUG,
-                                      recipes = recipes,
-                                      mutations = mutations)
-
-  @classmethod
-  def _setup_test(clazz, config, recipes = None):
-    root_dir = clazz._make_temp_dir()
-    am = clazz._make_test_artifact_manager(recipes = recipes)
-    config_filename = path.join(root_dir, 'config.revenv')
-    clazz._write_config_file(config_filename, config, am)
-
-    bt = BT.parse_path('linux-ubuntu-18/x86_64/release')
-    config_obj = venv_config.load(config_filename, bt)
-    vm = venv_manager(config_obj, am, bt, root_dir)
-    return _test_context(root_dir, config_filename, am, vm, bt)
-    
   @classmethod
   def _write_config_file(clazz, filename, config, artifact_manager):
     config_head = clazz._HEAD_TEMPLATE.format(artifacts_dir = artifact_manager._root_dir)
@@ -289,33 +270,5 @@ projects
   def _parse_stdout_list(clazz, s):
     return [ x.strip() for x in s.split('\n') if x.strip() ]
 
-
-class _test_context(namedtuple('_test_context', 'tmp_dir, config_filename, artifact_manager, manager, build_target')):
-
-  def __new__(clazz, tmp_dir, config_filename, artifact_manager, manager, build_target):
-    return clazz.__bases__[0].__new__(clazz, tmp_dir, config_filename, artifact_manager, manager, build_target)
-
-  def installed_packages(self, project_name, include_version = False):
-    return self.manager.installed_packages_names(project_name, self.build_target, include_version = include_version)
-
-  def update_from_config(self, project_name, options = None):
-    return self.manager.update_from_config(project_name, self.build_target, options = options)
-
-  def clear_project_from_config(self, project_name):
-    return self.manager.clear_project_from_config(project_name, self.build_target)
-  
-  def rewrite_config(self, config):
-    config_head = test_manager._HEAD_TEMPLATE.format(artifacts_dir = self.artifact_manager._root_dir)
-    content = config.format(head = config_head)
-    file_util.save(self.config_filename, content = content)
-    config_obj = venv_config.load(self.config_filename, self.build_target)
-    self.manager._config = config_obj
-
-  def publish_artifacts(self, recipes):
-    FPUT.artifact_manager_publish(self.artifact_manager, recipes = recipes)
-    
-  def clear_artifacts(self):
-    FPUT.artifact_manager_clear(self.artifact_manager)
-    
 if __name__ == '__main__':
   unit_test.main()
