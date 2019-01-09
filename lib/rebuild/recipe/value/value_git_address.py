fb2c38bfab1896f645fd80f69e197ef9a7543ac1
Ramiro Estrugo <ramiro@fateware.com>
2019-01-08 19:38:00 -0800

    add support for using [DIR] for the git address tag which will tarball the dir and use that instead.  very useful for testing before committing.

--- lib/rebuild/recipe/value/value_git_address.py
+++ lib/rebuild/recipe/value/value_git_address.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 import os.path as path
+from bes.system import logger
 from bes.common import check, string_util, time_util
 from bes.compat import StringIO
 from bes.git import git_address
@@ -9,14 +10,14 @@ from bes.key_value import key_value
 
 from .value_base import value_base
 
+
+_LOG = logger('value_git_address')
+
 class value_git_address(value_base):
 
   def __init__(self, origin = None, value = None, properties = None):
     super(value_git_address, self).__init__(origin, properties = properties)
-    if value:
-      check.check_git_address(value)
-    self.value = value
-    self.is_local_dir = False
+    self.value = check.check_git_address(value, allow_none = True)
     
   def __eq__(self, other):
     return self.value == other.value
@@ -62,18 +63,8 @@ class value_git_address(value_base):
     properties = clazz.parse_properties(rest)
     address = path.expanduser(address)
     if path.isdir(address):
-      if revision == 'HEAD':
-        revision = git.last_commit_hash(address, short_hash = True)
-        revision_timestamp = git.commit_timestamp(address, revision)
-        revision_version = time_util.timestamp(when = revision_timestamp,
-                                               milliseconds = False,
-                                               delimiter = '.',
-                                               timezone = True)
-      #elif revision == '[DIR]':
-        
+      revision = address.resolved_revision
     return clazz(origin = origin, value = git_address(address, revision), properties = properties)
-    #return clazz(origin = origin, value = value, properties = properties)
-    #return clazz(origin = origin, where = where, properties = properties)
   
   @classmethod
   #@abstractmethod
@@ -87,15 +78,26 @@ class value_git_address(value_base):
     return values[-1]
   
   def downloaded_tarball_path(self, recipe_env):
-    return recipe_env.git_downloads_manager.tarball_path(self.address, self.revision)
+    if self.revision == '[DIR]':
+      result = recipe_env.source_dir_zipballs.get_tarball(self.address)
+    else:
+      result = recipe_env.git_downloads_manager.tarball_path(self.address, self.revision)
+    _LOG.log_d('downloaded_tarball_path: address=%s; revision=%s => %s' % (self.address, self.revision, path.relpath(result)))
+    return result
   
   def download(self, recipe_env):
+    _LOG.log_d('downloaded: address=%s; revision=%s' % (self.address, self.revision))
+    if self.revision == '[DIR]':
+      return True
     if not recipe_env.git_downloads_manager.has_tarball(self.address, self.revision):
       assert self.needs_download(recipe_env)
       recipe_env.git_downloads_manager.get_tarball(self.address, self.revision)
     assert not self.needs_download(recipe_env)
 
   def needs_download(self, recipe_env):
+    _LOG.log_d('needs_downloaded: address=%s; revision=%s' % (self.address, self.revision))
+    if self.revision == '[DIR]':
+      return False
     return not path.isfile(self.downloaded_tarball_path(recipe_env))
     
 check.register_class(value_git_address, include_seq = False)
