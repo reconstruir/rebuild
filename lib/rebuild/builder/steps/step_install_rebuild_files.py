3d1276700d631c94dfe733f90f8fd3e57165069b
Ramiro Estrugo <ramiro@fateware.com>
2019-03-02 20:45:13 -0800

    add skip_install_rebuild_files for step_install_rebuild_files

--- lib/rebuild/builder/steps/step_install_rebuild_files.py
+++ lib/rebuild/builder/steps/step_install_rebuild_files.py
@@ -12,10 +12,14 @@ class step_install_rebuild_files(step):
 
   @classmethod
   def define_args(clazz):
-    return ''
+    return '''
+    skip_install_rebuild_files   bool    False
+    '''
   
   #@abstractmethod
   def execute(self, script, env, values, inputs):
+    if values.get('skip_install_rebuild_files'):
+      return self.result(True, 'skipping install_rebuild_files because skip_install_rebuild_files is True.')
     rebuild_dir = path.join(script.build_dir, 'rebuild')
     if not path.isdir(rebuild_dir):
       message = 'No rebuild files for %s' % (script.descriptor.full_name)
