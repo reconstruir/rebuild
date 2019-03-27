1e5e822d47307182c080e476c31fbb3c049cc68b
Ramiro Estrugo <ramiro@fateware.com>
2019-03-27 00:49:01 -0700

    use bes.properties

--- lib/rebuild/gradle/gradle_properties_file.py
+++ lib/rebuild/gradle/gradle_properties_file.py
@@ -3,21 +3,13 @@
 from os import path
 from bes.system import log
 from bes.common import check
-from bes.fs import file_util
-from bes.key_value import key_value, key_value_list
-from bes.text import text_line_parser
+from bes.properties.properties_file import properties_file
 
 class gradle_properties_file(object):
 
   @classmethod
   def read(clazz, filename):
-    check.check_string(filename)
-    content = file_util.read(filename)
-    lines = text_line_parser.parse_lines(content)
-    result = key_value_list()
-    for line in lines:
-      result.append(key_value.parse(line))
-    return result.to_dict()
+    return properties_file.read(filename, style = 'java')
 
   DEFAULT_FILENAME = path.expanduser('~/.gradle/gradle.properties')
   @classmethod
