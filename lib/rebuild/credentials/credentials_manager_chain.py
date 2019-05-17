62411efa51bc7ee822d830c15567e469d31985ac
Ramiro Estrugo <restrugo@imvu.com>
2019-05-16 22:55:58 -0700

    add.

---
+++ lib/rebuild/credentials/credentials_manager_chain.py
@@ -0,0 +1,27 @@
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
+
+from os import path
+
+from bes.common import check
+
+class credentials_manager_chain(object):
+
+  def __init__(self):
+    self._managers = []
+
+  def add_manager(self, manager):
+    check.check_credentials_manager(manager)
+    self._managers.append(manager)
+
+  def is_valid(self):
+    for manager in self._managers:
+      if manager.is_valid():
+        return True
+    return False
+    
+  def credentials(self):
+    for manager in self._managers:
+      if manager.is_valid():
+        return manager.credentials()
+      
+check.register_class(credentials_manager_chain, include_seq = False)
