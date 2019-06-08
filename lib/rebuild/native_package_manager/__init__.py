166d63f3b869330636ce1990f08bd49bd3f33345
Ramiro Estrugo <restrugo@imvu.com>
2019-06-07 20:51:44 -0700

    wip

--- lib/rebuild/native_package_manager/__init__.py
+++ lib/rebuild/native_package_manager/__init__.py
@@ -1,4 +1,1 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-
-from .native_package_manager import native_package_manager
+#-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
