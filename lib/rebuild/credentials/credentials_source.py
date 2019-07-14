1ed6258d7eb9e5e51739aaa433e394ed6dbc05ef
Ramiro Estrugo <restrugo@imvu.com>
2019-07-14 16:03:24 -0700

    remove unused code

--- lib/rebuild/credentials/credentials_source.py
+++ lib/rebuild/credentials/credentials_source.py
@@ -19,18 +19,3 @@ class credentials_source(with_metaclass(ABCMeta, object)):
   def credentials(self):
     pass
 check.register_class(credentials_source)
-  
-'''
-class credentials_source_env(credentials_source):
-
-  def __init__(self, username):
-    pass
-  
-  @abstractmethod
-  def is_valid(self):
-    pass
-
-  @abstractmethod
-  def credentials(self):
-    pass
-'''
