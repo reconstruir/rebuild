ebf962796370c85f0786ea25adc817aa38969888
Ramiro Estrugo <restrugo@imvu.com>
2019-08-20 23:21:26 -0700

    lots of work in progress to get the fs_artifactory implementation workin

--- lib/rebuild/artifactory/fs_artifactory.py
+++ lib/rebuild/artifactory/fs_artifactory.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
+from collections import namedtuple
+
 from os import path
 import json
 
@@ -72,26 +74,6 @@ class fs_artifactory(fs_base):
 
   #@abstractmethod
   def list_dir(self, remote_dir, recursive):
-    return self.list_dir_new(remote_dir, recursive)
-  
-  #@abstractmethod
-  def list_dir_old(self, remote_dir, recursive):
-    'List entries in a directory.'
-    remote_dir = file_util.ensure_lsep(remote_dir)
-
-    url = '{}/api/storage{}?list&deep=100&depth=1&listFolders=1'.format(self._address, remote_dir)
-
-    print('url: {}'.format(url))
-    
-    auth = ( self._credentials.username, self._credentials.password )
-    response = requests.get(url, auth = auth)
-    response_data = response.json()
-    entries = response_data['files']
-    children = fs_file_info_list([ self._convert_artifactory_entry_to_fs_tree(entry, depth = 0) for entry in entries ])
-    return fs_file_info(remote_dir, fs_file_info.DIR, None, None, None, children)
-
-  #@abstractmethod
-  def list_dir_new(self, remote_dir, recursive):
     'List entries in a directory.'
     remote_dir = file_util.ensure_lsep(remote_dir)
     remote_dir_no_leading_sep = file_util.lstrip_sep(remote_dir)
@@ -102,7 +84,8 @@ class fs_artifactory(fs_base):
     
     #print('         address: {}'.format(self._address))
     #print('      remote_dir: {}'.format(remote_dir))
-    #print(' decomposed_path: {}'.format(decomposed_path))
+#    print(' decomposed_path: {}'.format(decomposed_path))
+#    print(' decomposed_path len: {}'.format(len(decomposed_path)))
     #print('artifactory_repo: {}'.format(artifactory_repo))
     #print('    match_prefix: {}'.format(match_prefix))
 
@@ -115,7 +98,7 @@ class fs_artifactory(fs_base):
       'type': 'any',
     }
     if not recursive:
-      data['depth'] = '2'
+      data['depth'] = str(len(decomposed_path) + 1)
     data_json = json.dumps(data)
     aql_template = 'items.find({data_json}).include("*", "property.*")'
     aql = aql_template.format(data_json = data_json)
@@ -125,6 +108,7 @@ class fs_artifactory(fs_base):
     auth = ( self._credentials.username, self._credentials.password )
     response = requests.post(aql_url, data = aql, auth = auth)
     response_data = response.json()
+#    print(response_data)
     response_results = response_data.get('results', None)
     assert response_results
 
@@ -176,7 +160,7 @@ class fs_artifactory(fs_base):
       new_node = result.ensure_path(parts)
       setattr(new_node, '_entry', entry)
 
-    starting_node = result.find_child(lambda child: getattr(child, '_entry')['_remote_filename'] == file_util.lstrip_sep(remote_dir))
+    starting_node = result.find_child(lambda child: getattr(child, '_entry')['_remote_filename'] == remote_dir_no_leading_sep)
     assert starting_node
     fs_tree = self._convert_node_to_fs_tree(starting_node, depth = 0)
     return fs_tree
@@ -201,39 +185,72 @@ class fs_artifactory(fs_base):
       return True
     except fs_error as ex:
       return False
-  
+
   #@abstractmethod
   def file_info(self, remote_filename):
     'Get info for a single file.'
-
-    remote_filename = file_util.ensure_lsep(remote_filename)
-    if remote_filename == '/':
-      return fs_file_info(remote_filename, fs_file_info.DIR, None, None, None, fs_file_info_list())
-    
-    url = '{}/api/storage{}'.format(self._address, remote_filename)
-    print('url: {}'.format(url))
-    auth = ( self._credentials.username, self._credentials.password )
-    response = requests.get(url, auth = auth)
-    #print('response: {}'.format(response))
-    if response.status_code != 200:
-      raise fs_error('file not found: {}'.format(remote_filename))
-      
-    data = response.json()
-    if 'children' in data:
+    rd = self._parse_remote_filename(remote_filename)
+    storage_response = self._storage_query(rd)
+    if 'children' in storage_response:
       ftype = fs_file_info.DIR
     else:
       ftype = fs_file_info.FILE
-
     if ftype == fs_file_info.FILE:
-      checksum = data['checksums']['sha256']
-      attributes = {}
-      size = long(data['size'])
+      file_data = self._aql_query_file(rd)
+      attributes = self._parse_artifactory_properties(file_data.get('properties', None))
+      checksum = file_data['sha256']
+      size = long(file_data['size'])
     else:
       checksum = None
       attributes = None
       size = None
-      
     return fs_file_info(file_util.lstrip_sep(remote_filename), ftype, size, checksum, attributes, fs_file_info_list())
+
+  #@abstractmethod
+  def _aql_query_file(self, rd):
+    aql_url = '{address}/api/search/aql'.format(address = self._address)
+    data = {
+      'repo': rd.repo,
+      "path" : {
+        '$match': path.dirname(rd.prefix),
+      },
+      "name" : {
+        '$match': path.basename(rd.prefix),
+      },
+      'type': 'any',
+    }
+    data_json = json.dumps(data)
+    aql_template = 'items.find({data_json}).include("*", "property.*")'
+    aql = aql_template.format(data_json = data_json)
+    auth = ( self._credentials.username, self._credentials.password )
+    response = requests.post(aql_url, data = aql, auth = auth)
+    response_data = response.json()
+    results = response_data.get('results', None)
+    if not results:
+      raise fs_error('file not found: {}'.format(rd.remote_filename))
+    assert len(results) == 1
+    return results[0]
+
+  #@abstractmethod
+  def _storage_query(self, rd):
+    url = '{}/api/storage{}'.format(self._address, rd.remote_filename_sep)
+    auth = ( self._credentials.username, self._credentials.password )
+    response = requests.get(url, auth = auth)
+    if response.status_code != 200:
+      raise fs_error('file not found: {}'.format(remote_filename))
+    response_data = response.json()
+    return response_data
+  
+  def _get_file_properties(self, remote_filename):
+    'Get just the properties for a file.'
+
+    url = '{}/api/storage{}?properties'.format(self._address, remote_filename)
+    auth = ( self._credentials.username, self._credentials.password )
+    response = requests.get(url, auth = auth)
+    if response.status_code != 200:
+      raise fs_error('file not found: {}'.format(remote_filename))
+    data = response.json()
+    return data
 
   def _make_info(self, entry, children):
     is_file = entry['type'] != 'folder'
@@ -246,7 +263,7 @@ class fs_artifactory(fs_base):
       
     if ftype == fs_file_info.FILE:
       checksum = str(entry['sha256'])
-      attributes = {}
+      attributes = self._parse_artifactory_properties(entry.get('properties', None))
       size = entry['size']
     else:
       checksum = None
@@ -254,6 +271,28 @@ class fs_artifactory(fs_base):
       size = None
     return fs_file_info(file_util.lstrip_sep(remote_filename), ftype, size, checksum, attributes, children)
 
+  _PROP_KEY_BLACKLIST = [
+    'trash.',
+  ]
+
+  @classmethod
+  def _prop_key_is_black_listed(clazz, key):
+    for x in clazz._PROP_KEY_BLACKLIST:
+      if key.startswith(x):
+        return True
+    return False
+  
+  @classmethod
+  def _parse_artifactory_properties(clazz, properties):
+    if not properties:
+      return {}
+    result = {}
+    for prop in properties:
+      key = prop['key']
+      if not clazz._prop_key_is_black_listed(key):
+        result[key] = prop.get('value', None)
+    return result
+  
   #@abstractmethod
   def remove_file(self, filename):
     'Remove filename.'
@@ -273,3 +312,15 @@ class fs_artifactory(fs_base):
   def set_file_attributes(self, filename, attributes):
     'Set file attirbutes.'
     assert False
+
+  _parsed_remote_filename = namedtuple('_parsed_remote_filename', 'remote_filename, decomposed_path, remote_filename_sep, remote_filename_no_sep, repo, prefix')
+  @classmethod
+  def _parse_remote_filename(clazz, remote_filename):
+    'Parse a remote_filename and return the artifactory specific parts such as repo and prefix.'
+    remote_filename_sep = file_util.ensure_lsep(remote_filename)
+    remote_filename_no_sep = file_util.lstrip_sep(remote_filename)
+    repo = remote_filename.split('/')[0]
+    prefix = '/'.join(remote_filename_sep.split('/')[2:])
+    decomposed_path = file_path.decompose(remote_filename_sep)
+    return clazz._parsed_remote_filename(remote_filename, decomposed_path, remote_filename_sep,
+                                         remote_filename_no_sep, repo, prefix)
