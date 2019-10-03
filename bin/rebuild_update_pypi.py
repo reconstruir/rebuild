cb5517d4330ca509d727a505f3407f2acaa850b2
Ramiro Estrugo <restrugo@imvu.com>
2019-10-03 14:02:06 -0700

    fix variables

--- bin/rebuild_update_pypi.py
+++ bin/rebuild_update_pypi.py
@@ -4,18 +4,21 @@
 import argparse, os.path as path
 import pprint
 
-from bes.fs.file_replace import file_replace
-from bes.fs.temp_file import temp_file
 from bes.fs.file_util import file_util
-from bes.git.git_repo import git_repo
+from bes.fs.file_replace import file_replace
+from bes.key_value.key_value_list import key_value_list
 
 from rebuild.builder.builder_recipe_loader import builder_recipe_loader
+
 from rebuild.ingest.ingest_pypi import ingest_pypi
-from rebuild.recipe.recipe_load_env import testing_recipe_load_env
-from rebuild.recipe.recipe_data_manager import recipe_data_manager
+
 from rebuild.recipe.recipe_data_entry import recipe_data_entry
 from rebuild.recipe.recipe_data_entry import recipe_data_entry_list
+from rebuild.recipe.recipe_data_manager import recipe_data_manager
+from rebuild.recipe.recipe_load_env import testing_recipe_load_env
+from rebuild.recipe.value.masked_value import masked_value
 from rebuild.recipe.value.masked_value_list import masked_value_list
+from rebuild.recipe.value.value_key_values import value_key_values
 
 class update_cli(object):
   
@@ -48,47 +51,48 @@ class update_cli(object):
     env = testing_recipe_load_env()
     recipes = builder_recipe_loader.load(env, filename)
     for recipe in recipes:
-      system = 'linux' # doesnt matter just needs to be a valid system
-      vars_kvl = recipe.resolve_variables('linux')
-      vars_dict = vars_kvl.to_dict()
-      #print(vars_kvl.to_string(value_delimiter = '\n'))
-      upstream_name = vars_dict['_upstream_name']
-      pypi_data = ingest_pypi.project_info(upstream_name)
-      old_version = recipe.descriptor.version.upstream_version
-      new_version = pypi_data.latest_release.version
-      
-      if old_version == new_version:
-        print('{}: already at latest version "{}"'.format(filename, new_version))
-      else:
-        print('         name: {}'.format(recipe.descriptor.name))
-        print('upstream_name: {}'.format(upstream_name))
-        print('  old_version: {}'.format(old_version))
-        print('  new_version: {}'.format(new_version))
-
-        old_release = pypi_data.find_by_version(old_version)
-        new_release = pypi_data.latest_release
-
-        new_data = self.make_new_data(recipe.data, old_release, new_release)
-
-        mutations = {
-          'data': self.make_new_data(recipe.data, old_release, new_release),
-        }
-        if update:
-          mutations['descriptor'] = self.make_new_descriptor(recipe.descriptor, new_version)
-          
-        new_recipe = recipe.clone(mutations = mutations)
-        
-        file_util.backup(filename)
-
-        new_recipe.save_to_file(filename)
-        
+      self._process_recipe(recipe, filename, update)
     return 0
 
-  def make_new_descriptor(clazz, descriptor, new_upstream_version):
+  def _process_recipe(self, recipe, filename, update):
+    system = 'linux' # doesnt matter just needs to be a valid system
+    vars_kvl = recipe.resolve_variables('linux')
+    vars_dict = vars_kvl.to_dict()
+    #print(vars_kvl.to_string(value_delimiter = '\n'))
+    upstream_name = vars_dict['_upstream_name']
+    pypi_data = ingest_pypi.project_info(upstream_name)
+    old_version = recipe.descriptor.version.upstream_version
+    new_version = pypi_data.latest_release.version
+      
+    print('         name: {}'.format(recipe.descriptor.name))
+    print('upstream_name: {}'.format(upstream_name))
+    print('  old_version: {}'.format(old_version))
+    print('  new_version: {}'.format(new_version))
+
+    old_release = pypi_data.find_by_version(old_version)
+    new_release = pypi_data.latest_release
+
+    mutations = {
+      'data': self._make_new_data(recipe.data, old_release, new_release),
+      'variables': self._make_new_variables(recipe.variables, upstream_name),
+    }
+    if update:
+      mutations['descriptor'] = self._make_new_descriptor(recipe.descriptor, new_version)
+          
+    new_recipe = recipe.clone(mutations = mutations)
+        
+    file_util.backup(filename)
+    new_recipe.save_to_file(filename)
+    replacements = {
+      'all: ${_url}/${_filename} ingested_filename=${_ingested_filename} checksum=${_checksum}': 'all: ${_url} ingested_filename=${_ingested_filename} checksum=@{DATA:checksum:${_version}}',
+    }
+    file_replace.replace(filename, replacements, backup = False)
+  
+  def _make_new_descriptor(clazz, descriptor, new_upstream_version):
     new_version = descriptor.version.clone({ 'upstream_version': new_upstream_version })
     return descriptor.clone({ 'version': new_version })
 
-  def make_new_data(clazz, data, old_release, new_release):
+  def _make_new_data(clazz, data, old_release, new_release):
     dm = recipe_data_manager.from_masked_value_list(data)
     dm.append(recipe_data_entry('all', 'checksum', new_release.version, new_release.checksum))
     dm.append(recipe_data_entry('all', 'path_hash', new_release.version, new_release.path_hash))
@@ -97,6 +101,24 @@ class update_cli(object):
     dm.remove_dups()
     dm.sort_by_version()
     return masked_value_list([ recipe_data_manager.parse_entry_text(str(x)) for x in dm ])
+
+  def _make_new_variables(clazz, variables, upstream_name):
+    result = masked_value_list()
+
+    texts = [
+      '_version=${REBUILD_PACKAGE_UPSTREAM_VERSION}',
+      '_upstream_name={}'.format(upstream_name),
+      '_filename=${_upstream_name}-${_version}.tar.gz',
+      '_url=https://files.pythonhosted.org/packages/@{DATA:path_hash:${_version}}/${_filename}',
+      '_ingested_filename=python/packages/${_filename}',
+    ]
+    result = masked_value_list()
+    for text in texts:
+      kvl = key_value_list.parse(text)
+      value = value_key_values(origin = None, value = kvl)
+      mvl = masked_value('all', value)
+      result.append(mvl)
+    return result
   
 if __name__ == '__main__':
   update_cli.run()
