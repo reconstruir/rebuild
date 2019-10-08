b4d675e95c6e0a745443eee0fcb1ade27d5a5028
Ramiro Estrugo <restrugo@imvu.com>
2019-10-07 19:28:07 -0700

    add command to fix requirement versions

--- lib/rebuild/recipe/recipe_cli_args.py
+++ lib/rebuild/recipe/recipe_cli_args.py
@@ -6,6 +6,7 @@ import os
 from bes.common.check import check
 
 from .recipe_cli_command import recipe_cli_command
+from rebuild.builder.builder_config import builder_config
 
 class recipe_cli_args(object):
 
@@ -16,10 +17,21 @@ class recipe_cli_args(object):
 
     default_where = os.getcwd()
     
-    # create
+    # find
     p = subparser.add_parser('find', help = 'Find recipes.')
     p.add_argument('where', action = 'store', default = [ default_where ], nargs = '*',
                    help = 'Where to look for recipes. [ {} ]'.format(default_where))
     
+    # fix_requirements_versions
+    p = subparser.add_parser('fix_requirements_versions', help = 'Fix the requirements versions.')
+    p.add_argument('where', action = 'store', default = [ default_where ], nargs = '*',
+                   help = 'Where to look for recipes. [ {} ]'.format(default_where))
+    p.add_argument('--python-version', action = 'store', default = builder_config.DEFAULT_PYTHON_VERSION,
+                   help = 'The version of python to use. [ {} ]'.format(builder_config.DEFAULT_PYTHON_VERSION))
+    
   def _command_find(self, where):
     return recipe_cli_command.find(where)
+
+  def _command_fix_requirements_versions(self, where, python_version):
+    return recipe_cli_command.fix_requirements_versions(where, python_version)
+  
--- lib/rebuild/recipe/recipe_cli_command.py
+++ lib/rebuild/recipe/recipe_cli_command.py
@@ -1,13 +1,13 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
-#from bes.git.git_repo import git_repo
-#from bes.archive.archiver import archiver
-#from bes.archive.archive_util import archive_util
-
 from bes.cli.cli_helper import cli_helper
 from bes.fs.file_find import file_find
+from bes.fs.file_util import file_util
 
+from rebuild.builder.builder_recipe_loader import builder_recipe_loader
 from rebuild.recipe.recipe import recipe
+from rebuild.recipe.recipe_load_env import testing_recipe_load_env
+from rebuild.base.requirement_list import requirement_list
 
 class recipe_cli_command(object):
   
@@ -19,5 +19,52 @@ class recipe_cli_command(object):
     return 0
 
   @classmethod
+  def fix_requirements_versions(clazz, where, python_version):
+    recipe_filenames = clazz._find_recipes(where)
+    recipes = clazz._load_recipes(recipe_filenames, python_version)
+    for recipe in recipes.values():
+      clazz._fix_one_requirements_versions(recipe, recipes)
+    return 0
+
+  @classmethod
+  def _fix_one_requirements_versions(clazz, recipe, other_recipes):
+    new_requirements = requirement_list()
+    for req in recipe.requirements:
+      assert req.operator in [ '==', '>=' ]
+      req_recipe = other_recipes[req.name]
+      old_version = req.version
+      new_version = req_recipe.upstream_version
+      if old_version != new_version:
+        new_req = req.clone(mutations = { 'version': new_version })
+        new_requirements.append(new_req)
+      else:
+        new_requirements.append(req)
+    if new_requirements != recipe.requirements:
+      print('    FIX: {}: name={} old={} new={}'.format(recipe.filename, recipe.name, recipe.requirements, new_requirements))
+      new_recipe = recipe.clone(mutations = { 'requirements': new_requirements })
+      file_util.backup(new_recipe.filename)
+      new_recipe.save_to_file(new_recipe.filename)
+      
+  @classmethod
   def _find_recipes(clazz, where):
-    return cli_helper.resolve_files(where, func = recipe.is_recipe)
+    return cli_helper.resolve_files(where, func = clazz._is_recipe)
+
+  @classmethod
+  def _is_recipe(clazz, filename):
+    return recipe.is_recipe(filename) and not filename.lower().endswith('.bak')
+
+  @classmethod
+  def _load_recipes(clazz, recipe_filenames, python_version):
+    env = testing_recipe_load_env()
+    env.variable_manager.add_variable('REBUILD_PYTHON_VERSION', python_version)
+    result = {}
+    for filename in recipe_filenames:
+      recipes = builder_recipe_loader.load(env, filename)
+      for recipe in recipes:
+        name = recipe.descriptor.name
+        if name in result:
+          raise RuntimeError('duplicate recipe for "{}":\n  {}\n  {}'.format(name,
+                                                                             recipe.filename,
+                                                                             result[name].filename))
+        result[name] = recipe
+    return result
