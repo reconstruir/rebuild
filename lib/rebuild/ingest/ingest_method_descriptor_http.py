bb748a3c2242a8e4b17b1dbae7890de145a4f39d
Ramiro Estrugo <restrugo@imvu.com>
2019-12-23 09:53:00 -0800

    treat cookies as a dict since thats what the http library expects.

--- lib/rebuild/ingest/ingest_method_descriptor_http.py
+++ lib/rebuild/ingest/ingest_method_descriptor_http.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 from bes.url.http_download_cache import http_download_cache
+from bes.key_value.key_value_list import key_value_list
 
 from .ingest_method_descriptor_base import ingest_method_descriptor_base
 from .ingest_method_field import ingest_method_field
@@ -25,7 +26,9 @@ class ingest_method_descriptor_http(ingest_method_descriptor_base):
     url = self._check_download_field(args, 'url')
     checksum = self._check_download_field(args, 'checksum')
     ingested_filename = self._check_download_field(args, 'ingested_filename')
-    cookies = self._check_download_field(args, 'cookies', optional = True)
+    cookies_str = self._check_download_field(args, 'cookies', optional = True)
+    cookies = key_value_list.parse(cookies_str).to_dict()
     cache_dir = self._check_download_field(args, 'cache_dir')
     cache = http_download_cache(cache_dir)
+    print('url: {}'.format(url))
     return cache.get_url(url, checksum = checksum, cookies = cookies)
