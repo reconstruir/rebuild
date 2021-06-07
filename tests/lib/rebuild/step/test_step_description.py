31707e4808e0c87f0f35faf6b246c9296d895b2d
Ramiro Estrugo <git@fateware.com>
2021-06-06 20:01:31 -0700

    comment out broken test

--- tests/lib/rebuild/step/test_step_description.py
+++ tests/lib/rebuild/step/test_step_description.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 #
-import unittest
-from rebuild.step.step import step
-from rebuild.step.step_description import step_description
-
-class test_step_description(unittest.TestCase):
-
-  class sample_step_save_args1(step):
-    def __init__(self):
-      super(test_step_description.sample_step_save_args1, self).__init__()
-    def execute(self, args):
-      return step_result(True, None)
-
-  class sample_step_save_args2(step):
-    def __init__(self):
-      super(test_step_description.sample_step_save_args2, self).__init__()
-    def execute(self, args):
-      return step_result(True, None)
-
-  class sample_step_save_args3(step):
-    def __init__(self):
-      super(test_step_description.sample_step_save_args3, self).__init__()
-    def execute(self, args):
-      return step_result(True, None)
-
-  class test_step4(step):
-    def __init__(self):
-      super(test_step_description.test_step4, self).__init__()
-    def execute(self, args):
-      return step_result(True, None)
-
-if __name__ == '__main__':
-  unittest.main()
+###### import unittest
+###### from rebuild.step.step import step
+###### from rebuild.step.step_description import step_description
+###### 
+###### class test_step_description(unittest.TestCase):
+###### 
+######   class sample_step_save_args1(step):
+######     def __init__(self):
+######       super(test_step_description.sample_step_save_args1, self).__init__()
+######     def execute(self, args):
+######       return step_result(True, None)
+###### 
+######   class sample_step_save_args2(step):
+######     def __init__(self):
+######       super(test_step_description.sample_step_save_args2, self).__init__()
+######     def execute(self, args):
+######       return step_result(True, None)
+###### 
+######   class sample_step_save_args3(step):
+######     def __init__(self):
+######       super(test_step_description.sample_step_save_args3, self).__init__()
+######     def execute(self, args):
+######       return step_result(True, None)
+###### 
+######   class test_step4(step):
+######     def __init__(self):
+######       super(test_step_description.test_step4, self).__init__()
+######     def execute(self, args):
+######       return step_result(True, None)
+###### 
+###### if __name__ == '__main__':
+######   unittest.main()
