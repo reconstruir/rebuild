8717377fc478565c020408e11ff63fcf62a4e8b4
Ramiro Estrugo <git@fateware.com>
2022-04-09 16:22:25 -0700

    update for changes in bes

--- lib/rebuild/venv/venv_shell_script.py
+++ lib/rebuild/venv/venv_shell_script.py
@@ -1,8 +1,8 @@
 #-*- coding:utf-8; mode:python; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-
 
 import copy, os.path as path
-from bes.common.string_util import string_util
 from bes.fs.file_util import file_util
+from bes.text.text_replace import text_replace
 
 class venv_shell_script(object):
 
@@ -17,7 +17,7 @@ class venv_shell_script(object):
   def save(self, root_dir, variables, only_if_not_there = False):
     filename = path.join(root_dir, self.basename)
     variables = copy.deepcopy(variables)
-    content = string_util.replace(self.template, variables)
+    content = text_replace.replace(self.template, variables)
     content = content.replace(path.expanduser('~'), '${HOME}')
     if self._content_changed(filename, content):
       if only_if_not_there and path.isfile(filename):
