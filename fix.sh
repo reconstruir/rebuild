ad2b176ba6ad26e146c0c0fc1eebfb09dc271129
Ramiro Estrugo <github_reconstruir@fateware.com>
2026-07-21 06:59:43 -0700

    more fix

--- fix.sh
+++ fix.sh
@@ -14,5 +14,4 @@ _fix "from bes.system.compat import with_metaclass" ""
 _fix "bes.fs.file_path import file_path" "bes.files.bf_path import bf_path"
 _fix "file_path." "bf_path."
 _fix "from bes.fs import bf_check" "from bes.files.bf_check import bf_check"
-
-
+_fix "from bes.files.bf_checksum_getter_raw import file_checksum_getter_raw" "from bes.fs.file_checksum_getter_raw import file_checksum_getter_raw"
